8051712a636974248eb914fac62da9cfdd84ac35
Surya Jakhotia <surya.jakhotia1@t-mobile.com>
2017-11-21 14:41:20 -0800

    remove the unused cognito authorizer

--- platform_cognito_authorizer/test/test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('chai').assert;
-const index = require('../index');
-
-describe('Sample', function() {
-    it('tests handler', function(done) {
-
-        done();
-    });
-});
