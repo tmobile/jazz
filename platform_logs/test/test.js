e215b19b7c6c3b40b579f8197612d3176042000c
Daimen Williams <daimenwill@gmail.com>
2017-12-06 02:41:00 -0800

    input validation tests done for CAPI962, 33% index coverage

--- platform_logs/test/test.js
+++ platform_logs/test/test.js
@@ -1,11 +1,239 @@
-const assert = require('chai').assert;
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
 const index = require('../index');
+const awsContext = require('aws-lambda-mock-context');
+const sinon = require('sinon');
+const logger = require("../components/logger.js");
+//const AWS = require("aws-sdk-mock");
 
-describe('Sample', function() {
-  it('tests handler', function(done) {
+describe('platform_logs', function() {
+  var event, context, callback, spy, stub, logStub, logMessage, errorMessage, errorType;
 
-    // Add your test cases here.
-    assert(true);
-    done();
+  //setup a helper function to check for expected outputs given different input parameters
+  var inputValidation = (eventProp, eventProp2, newValue, errMessage, errType) => {
+    //if there is a second prop defined, than the value to be changed is in a nested object
+    if(eventProp2){
+      event[eventProp][eventProp2] = newValue;
+    }
+    else if(eventProp){
+      event[eventProp] = newValue;
+    }
+    else{
+      event = newValue;
+    }
+    //check if handler returns error notification with expected error type and message
+    var handlerResponse = index.handler(event, context, callback);
+    var bool = handlerResponse.includes(errMessage) && handlerResponse.includes(errType);
+    return bool;
+  }
+
+  //setup additional helper function for validating multiple values
+  var multipleValidation = (prop1, prop2, valueArray, errMessage, errType) => {
+    var allChecks = true;
+    //allChecks stays true if handler() returns expected error for every value assigned
+    for(i in valueArray){
+      var bool = inputValidation(prop1, prop2, valueArray[i], errMessage, errType);
+      if(!bool){
+        allChecks = false;
+        break;
+      }
+    }
+    return allChecks;
+  }
+
+  beforeEach(function(){
+    //setup spy to wrap async/extraneous functions
+    spy = sinon.spy();
+    event = {
+      "stage" : "test",
+      "body_get":{},
+      "body_put":{
+        "endpoints" : {
+          "dev": "newval"
+        }
+      },
+      "body":{
+        "environment": "test",
+        "service": "dAnCe",
+        "domain": "bA11r00m",
+        "category": "api",
+        "offset": "44",
+        "type" : "debug"
+      },
+      "method":"POST",
+      "path" : {},
+      "path_put" : {
+        "id": "88e046fe-1acc-5f30-0dff-d7c2c5357621"
+      },
+      "query_basic" : {
+        "limit":"10",
+        "offset":"0"
+      },
+      "query_put" : {
+        "limit":"10",
+        "offset":"0",
+        "status": "Active"
+      },
+      "query_get" : {
+        "limit":"10",
+        "offset":"0",
+        "status": "Active,inactive"
+      },
+      "principalId": "i@m"
+    };
+    context = awsContext();
+    callback = (err, responseObj) => {
+      if(err){
+        return err;
+      }
+      else{
+        return JSON.stringify(responseObj);
+      }
+    };
+    err = {
+      "errorType" : "Intrntnl",
+      "message" : "latin standard"
+    };
+  });
+
+  /*
+  * Given a falsy event, handler() indicates that an invalid request was made
+  * @param{object} event -> either null, undefined, or ""
+  * @params{object, function} aws context, and callback function as described in beforeEach
+  * @returns{string} error message indicating a bad request was made
+  */
+  it('should inform of bad request if not given any event info', function() {
+    errorMessage = "Invalid request to process for logs API";
+    errorType = "BadRequest";
+    var invalidArray = ["", null, undefined];
+    var allChecks = multipleValidation(null, null, invalidArray, errorMessage, errorType);
+    assert.isTrue(allChecks);
+  });
+
+  /*
+  * Given an event.method != POST, handler() indicates that an invalid request was made
+  * @param{object} event -> event.method = null, undefined, "" or != POST
+  * @params{object, function} aws context, and callback function as described in beforeEach
+  * @returns{string} error message indicating a bad request was made
+  */
+  it('should inform of bad request if given an event.method other than "POST" or no event.method', function() {
+    errorMessage = "Invalid request to process for logs API";
+    errorType = "BadRequest";
+    var invalidArray = ["GET", "PUT", "DELETE", "HEAD", "OPTIONS", "", null, undefined];
+    var allChecks = multipleValidation("method", null, invalidArray, errorMessage, errorType);
+    assert.isTrue(allChecks);
+  });
+
+  /*
+  * Given no event.body, handler() indicates that inputs are missing
+  * @param{object} event -> event.body = null, undefined, or ""
+  * @params{object, function} aws context, and callback function as described in beforeEach
+  * @returns{string} error message indicating a bad request was made
+  */
+  it('should inform of bad request if event.body is falsy', function() {
+    errorMessage = "Service inputs not defined!";
+    errorType = "BadRequest";
+    var invalidArray = ["", null, undefined];
+    var allChecks = multipleValidation("body", null, invalidArray, errorMessage, errorType);
+    assert.isTrue(allChecks);
+  });
+
+  /*
+  * Given no event.body.service, handler() indicates that service name is missing
+  * @param{object} event -> event.body.service = null, undefined, or ""
+  * @params{object, function} aws context, and callback function as described in beforeEach
+  * @returns{string} error message indicating a bad request was made
+  */
+  it('should inform of bad request if event.body.service is falsy', function() {
+    errorMessage = "missing required input parameter service name.";
+    errorType = "BadRequest";
+    var invalidArray = ["", null, undefined];
+    var allChecks = multipleValidation("body", "service", invalidArray, errorMessage, errorType);
+    assert.isTrue(allChecks);
+  });
+
+  /*
+  * Given no event.body.domain, handler() indicates that domain identifier is missing
+  * @param{object} event -> event.body.domain = null, undefined, or ""
+  * @params{object, function} aws context, and callback function as described in beforeEach
+  * @returns{string} error message indicating a bad request was made
+  */
+  it('should inform of bad request if event.body.domain is falsy', function() {
+    errorMessage = "missing required input parameter domain.";
+    errorType = "BadRequest";
+    var invalidArray = ["", null, undefined];
+    var allChecks = multipleValidation("body", "domain", invalidArray, errorMessage, errorType);
+    assert.isTrue(allChecks);
+  });
+
+  /*
+  * Given no event.body.environment, handler() indicates that environment identifier is missing
+  * @param{object} event -> event.body.environment = null, undefined, or ""
+  * @params{object, function} aws context, and callback function as described in beforeEach
+  * @returns{string} error message indicating a bad request was made
+  */
+  it('should inform of bad request if event.body.environment is falsy', function() {
+    errorMessage = "missing required input parameter environment.";
+    errorType = "BadRequest";
+    var invalidArray = ["", null, undefined];
+    var allChecks = multipleValidation("body", "environment", invalidArray, errorMessage, errorType);
+    assert.isTrue(allChecks);
+  });
+
+  /*
+  * Given no event.body.category, handler() indicates that category identifier is missing
+  * @param{object} event -> event.body.category = null, undefined, or ""
+  * @params{object, function} aws context, and callback function as described in beforeEach
+  * @returns{string} error message indicating a bad request was made
+  */
+  it('should inform of bad request if event.body.category is falsy', function() {
+    errorMessage = "missing required input parameter category.";
+    errorType = "BadRequest";
+    var invalidArray = ["", null, undefined];
+    var allChecks = multipleValidation("body", "category", invalidArray, errorMessage, errorType);
+    assert.isTrue(allChecks);
+  });
+
+  /*
+  * Given an event.body.environment not listed as valid through config, handler() indicates environment isn't valid
+  * @param{object} event -> event.body.environment not listed as valid in config file
+  * @params{object, function} aws context, and callback function as described in beforeEach
+  * @returns{string} error message indicating a bad request was made
+  */
+  it("should inform of bad request if event.body.environment doesn't match valid config values", () => {
+    errorMessage = "Only following values are allowed for environment - ";
+    errorType = "BadRequest";
+    var invalidArray = ["wa1tz", "tang0", "f0xtr0t"];
+    var allChecks = multipleValidation("body", "environment", invalidArray, errorMessage, errorType);
+    assert.isTrue(allChecks);
+  });
+
+  /*
+  * Given an event.body.category not listed as valid through config, handler() indicates category isn't valid
+  * @param{object} event -> event.body.category not listed as valid in config file
+  * @params{object, function} aws context, and callback function as described in beforeEach
+  * @returns{string} error message indicating a bad request was made
+  */
+  it("should inform of bad request if event.body.category doesn't match valid config values", () => {
+    errorMessage = "Only following values are allowed for category - ";
+    errorType = "BadRequest";
+    var invalidArray = ["chaCHA", "rUmBa", "sAmBa"];
+    var allChecks = multipleValidation("body", "category", invalidArray, errorMessage, errorType);
+    assert.isTrue(allChecks);
+  });
+
+  /*
+  * Given an event.body.type not listed as valid through config or is not given, handler() indicates type isn't valid
+  * @param{object} event -> event.body.type = falsy or is not listed as valid in config file
+  * @params{object, function} aws context, and callback function as described in beforeEach
+  * @returns{string} error message indicating a bad request was made
+  */
+  it("should inform of bad request if event.body.type doesn't match valid config values or isn't provided", () => {
+    errorMessage = "Only following values are allowed for logger type - ";
+    errorType = "BadRequest";
+    var invalidArray = ["sw1ng", "mAmb0", "", null, undefined];
+    var allChecks = multipleValidation("body", "type", invalidArray, errorMessage, errorType);
+    assert.isTrue(allChecks);
   });
 });
