43e375cb8acd0e98a2b742420fbaacf7585af2e7
SWilson162 <sini.wilson162@t-mobile.com>
2017-12-19 16:03:30 +0530

    Removing auth_token

--- create-serverless-service/test/test.js
+++ create-serverless-service/test/test.js
@@ -70,11 +70,11 @@ describe('create-serverless-service', function() {
     //set up for default valid values to pass into handler()
     beforeEach(function(){
       event = {
-        "stage" : "test",
-        "headers" : {
-          "Authorization" : "fr1end$hip_1s_mAg1c"
-        },
+        "stage" : "test"
         "principalId" : "@pp1eJack",
+		"headers" : {
+          "Authorization" : "pr!ncessBubb1eGum"
+        },
         "body" : {
           "service_name"	: "test-service",
           "service_type"	: "lambda",
@@ -148,22 +148,6 @@ describe('create-serverless-service', function() {
           allCases = false;
         }
       }
-      assert.isTrue(allCases);
-    });
-
-    /*
-    * Given an event with missing headers/headers.Authorization, handler() should throw InternalServerError
-    * @param {object} event, contains a null or undefined headers or headers.Authorization
-    * @params {object, function} default aws context and callback function as assigned above respectively
-    * @returns index.handler() should return an InternalServerError notification
-    */
-    it("should throw an InternalServerError error if missing event.headers", function(){
-      var errMessage = "'headers' is missing";
-      var errType = "BadRequest";
-      var allCases = checkCase("headers", "Authorization", null, errMessage, errType) &&
-                      checkCase("headers", "Authorization", undefined, errMessage, errType) &&
-                      checkCase("headers", null, null, errMessage, errType) &&
-                      checkCase("headers", null, undefined, errMessage, errType);
       assert.isTrue(allCases);
     });
 
