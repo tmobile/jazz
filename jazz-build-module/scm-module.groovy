f162a4157c9bbbc0c80c0cbd78aaeb6c48974b04
Surya Jakhotia <surya.jakhotia1@t-mobile.com>
2018-02-06 16:19:40 -0800

    :sparkles: add webhook method + cleanup

--- jazz-build-module/scm-module.groovy
+++ jazz-build-module/scm-module.groovy
@@ -1,8 +1,7 @@
 #!groovy?
-import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 import groovy.transform.Field
-
+import groovy.json.JsonSlurper
 /*
 * Module that handles managing projects (create, delete) in the user's preferred scm
 */
@@ -23,16 +22,17 @@ def initialize(configData){
 }
 
 def setRepoEndpoints(){
-    scm_user_services_api_endpoint = = "${scm_protocol}${scm_config.REPOSITORY.REPO_BASE}/rest/api/1.0/projects/${scm_config.REPOSITORY.REPO_LOC}/repos"
+    scm_user_services_api_endpoint = "${scm_protocol}${scm_config.REPOSITORY.REPO_BASE}/rest/api/1.0/projects/${scm_config.REPOSITORY.REPO_LOC}/repos"
     scm_core_services_endpoint = "${scm_protocol}${scm_config.REPOSITORY.REPO_BASE}/scm/${scm_config.REPOSITORY.REPO_CORE}/" 
     scm_user_services_clone_url = "${scm_config.REPOSITORY.REPO_BASE}/scm/${scm_config.REPOSITORY.REPO_LOC}/"
     scm_webhook_api = "${scm_protocol}${scm_config.REPOSITORY.REPO_BASE}/rest/webhook/1.0/projects/${scm_config.REPOSITORY.REPO_LOC}/repos/"
-    scm_branch_permission_api_endpoint =  "${scm_protocol}${scm_config.REPOSITORY.REPO_BASE}/rest/branch-permissions/2.0/projects/${scm_config.REPOSITORY.REPO_LOC}/repos/"
-    serviceonboarding_repo 	= "${scm_protocol}${scm_config.REPOSITORY.REPO_BASE}/scm/${scm_config.REPOSITORY.REPO_CORE}/service-onboarding-build-pack.git"
+    scm_branch_permission_api_endpoint = "${scm_protocol}${scm_config.REPOSITORY.REPO_BASE}/rest/branch-permissions/2.0/projects/${scm_config.REPOSITORY.REPO_LOC}/repos/"
+    serviceonboarding_repo	= "${scm_protocol}${scm_config.REPOSITORY.REPO_BASE}/scm/${scm_config.REPOSITORY.REPO_CORE}/service-onboarding-build-pack.git"
 }
 
 def createProject(repo_owner, repo_name){
     try { 
+        echo "ready to create project"
         if(scm_config.JAZZ_SCM == "gitlab") {
             def git_username
             def cas_proj_id
@@ -43,10 +43,10 @@ def createProject(repo_owner, repo_name){
                 git_username = repo_owner.substring(0, owner.indexOf("@"))
             }
 
-            def user_id = getGitlabUserId(git_username, ${config.GITLAB.gitlab_private_token})
+            def user_id = getGitlabUserId(git_username, ${scm_config.GITLAB.gitlab_private_token})
             
             def gitlab_repo_output = sh (
-                script: "curl --header \"Private-Token: ${config.GITLAB.gitlab_private_token}\" -X POST \"http://${scm_config.REPOSITORY.REPO_BASE}/api/v3/projects/user/$user_id?name=$repo_name&path=$repo_name&visibility=private&request_access_enabled=true\"",
+                script: "curl --header \"Private-Token: ${scm_config.GITLAB.gitlab_private_token}\" -X POST \"http://${scm_config.REPOSITORY.REPO_BASE}/api/v3/projects/user/$user_id?name=$repo_name&path=$repo_name&visibility=private&request_access_enabled=true\"",
                 returnStdout: true
             ).trim()
 
@@ -63,8 +63,8 @@ def createProject(repo_owner, repo_name){
 
             transferProject(cas_repo_id, cas_proj_id)
         }
-        else 
-            sh "curl -X POST -k -v -u \"${config.BITBUCKET.BITBUCKET_USERNAME}:${config.BITBUCKET.BITBUCKET_PASSWORD}\" -H \"Content-Type: application/json\" ${scm_user_services_api_endpoint} -d \'{\"name\":\""${repo_name}"\", \"scmId\": \"git\", \"forkable\": \"true\"}\'"
+        else{
+            sh "curl -X POST -k -v -u \"${scm_config.BITBUCKET.BITBUCKET_USERNAME}:${scm_config.BITBUCKET.BITBUCKET_PASSWORD}\" -H \"Content-Type: application/json\" " + scm_user_services_api_endpoint + " -d \'{\"name\":\""+ repo_name +"\", \"scmId\": \"git\", \"forkable\": \"true\"}\'"
         }
     }catch (ex) {
         if(!((ex.getMessage()).indexOf("groovy.json.internal.LazyMap") > -1)) {
@@ -77,11 +77,11 @@ def createProject(repo_owner, repo_name){
 def getGitlabUserId(gitlab_username){
     try{
         def output = sh (
-            script: "curl --header \"Private-Token: ${config.GITLAB.gitlab_private_token}\" -X GET \"http://${scm_config.REPOSITORY.REPO_BASE}/api/v3/users?username=$gitlab_username\"",
+            script: "curl --header \"Private-Token: ${scm_config.GITLAB.gitlab_private_token}\" -X GET \"http://${scm_config.REPOSITORY.REPO_BASE}/api/v3/users?username=$gitlab_username\"",
             returnStdout: true
         ).trim()
   
-        def jsonSlurper = new JsonSlurper()
+        def jsonSlurper = new groovy.json.JsonSlurper()
         def userObject = jsonSlurper.parseText(output)
   
         if(userObject == null || userObject.equals("") || userObject[0] == null || userObject[0].equals("") 
@@ -102,7 +102,7 @@ def getGitlabUserId(gitlab_username){
 def getCasRepoId(repo_loc){
     try{
         def output = sh (
-            script: "curl --header \"Private-Token: ${config.GITLAB.gitlab_private_token}\" -X GET \"http://${scm_config.REPOSITORY.REPO_BASE}/api/v3/groups?search=$repo_loc\"",
+            script: "curl --header \"Private-Token: ${scm_config.GITLAB.gitlab_private_token}\" -X GET \"http://${scm_config.REPOSITORY.REPO_BASE}/api/v3/groups?search=$repo_loc\"",
             returnStdout: true
         ).trim()
 
@@ -143,11 +143,10 @@ def setRepoPermissions(repo_owner, repo_name, admin_group) {
     sh "curl -X PUT -G -k -v -u \"${scm_config.BITBUCKET.BITBUCKET_USERNAME}:${scm_config.BITBUCKET.BITBUCKET_PASSWORD}\" -d \"name=$encoded_creator\" \"${scm_user_services_api_endpoint}/${repo_name}/permissions/users?permission=REPO_ADMIN\""
 }
 
-/**
- * Delete the project repository from scm
- * @param  scm_repo_name
- * @return
- */
+def addWebhook(repo_name, webhookName, targetUrl) {
+    sh "curl -X PUT -k -v -u \"${scm_config.BITBUCKET.BITBUCKET_USERNAME}:${scm_config.BITBUCKET.BITBUCKET_PASSWORD}\" -H \"Content-Type: application/json\" ${scm_webhook_api}${repo_name}/configurations  -d \'{\"title\": \"${webhookName}\", \"url\": \"${targetUrl}\" , \"enabled\": true}\'"
+}
+
 def deleteProject(repo_loc, repo_name) {
     if(scm_config.JAZZ_SCM == "gitlab"){
         def encodedProjectPath = URLEncoder.encode("${repo_loc}+${repo_name}", "utf-8")
