248159015164f13b8a589997ba464020a58fe040
Surya Jakhotia <surya.jakhotia1@t-mobile.com>
2018-02-15 15:52:48 -0800

    standardize on gitlab userid regex

--- jazz-build-module/scm-module.groovy
+++ jazz-build-module/scm-module.groovy
@@ -10,7 +10,7 @@ import groovy.json.JsonSlurper
 def scm_core_services_endpoint
 def scm_user_services_api_endpoint
 def scm_user_services_clone_url
-def scm_webhook_api
+
 def scm_branch_permission_api_endpoint
 
 def serviceonboarding_repo
@@ -27,14 +27,12 @@ def setRepoEndpoints(){
         scm_user_services_api_endpoint = "${scm_protocol}${scm_config.REPOSITORY.BASE_URL}/rest/api/1.0/projects/${scm_config.REPOSITORY.REPO_BASE_SERVICES}/repos/"
         scm_core_services_endpoint = "${scm_protocol}${scm_config.REPOSITORY.BASE_URL}/${scm_config.REPOSITORY.REPO_BASE_PLATFORM}/" 
         scm_user_services_clone_url = "${scm_protocol}${scm_config.REPOSITORY.BASE_URL}/${scm_config.REPOSITORY.REPO_BASE_SERVICES}/"
-        scm_webhook_api = "${scm_protocol}${scm_config.REPOSITORY.BASE_URL}/rest/webhook/1.0/projects/${scm_config.REPOSITORY.REPO_BASE_SERVICES}/repos/"
         scm_branch_permission_api_endpoint = "${scm_protocol}${scm_config.REPOSITORY.BASE_URL}/rest/branch-permissions/2.0/projects/${scm_config.REPOSITORY.REPO_BASE_SERVICES}/repos/"
         serviceonboarding_repo	= "${scm_protocol}${scm_config.REPOSITORY.BASE_URL}/${scm_config.REPOSITORY.REPO_BASE_PLATFORM}/service-onboarding-build-pack.git"
     }else if(scm_config.SCM.TYPE == "bitbucket"){
         scm_user_services_api_endpoint = "${scm_protocol}${scm_config.REPOSITORY.BASE_URL}/rest/api/1.0/projects/${scm_config.REPOSITORY.REPO_BASE_SERVICES}/repos/"
         scm_core_services_endpoint = "${scm_protocol}${scm_config.REPOSITORY.BASE_URL}/scm/${scm_config.REPOSITORY.REPO_BASE_PLATFORM}/" 
         scm_user_services_clone_url = "${scm_protocol}${scm_config.REPOSITORY.BASE_URL}/scm/${scm_config.REPOSITORY.REPO_BASE_SERVICES}/"
-        scm_webhook_api = "${scm_protocol}${scm_config.REPOSITORY.BASE_URL}/rest/webhook/1.0/projects/${scm_config.REPOSITORY.REPO_BASE_SERVICES}/repos/"
         scm_branch_permission_api_endpoint = "${scm_protocol}${scm_config.REPOSITORY.BASE_URL}/rest/branch-permissions/2.0/projects/${scm_config.REPOSITORY.REPO_BASE_SERVICES}/repos/"
         serviceonboarding_repo	= "${scm_protocol}${scm_config.REPOSITORY.BASE_URL}/scm/${scm_config.REPOSITORY.REPO_BASE_PLATFORM}/service-onboarding-build-pack.git"
     }
@@ -43,16 +41,14 @@ def setRepoEndpoints(){
 def createProject(repo_owner, repo_name){
     try { 
         if(scm_config.SCM.TYPE == "gitlab"){
-            def git_username
+            //gitlabs username is restricted to alphanumeric and . _ - characters, 
+            // so using email all email characters (except -, _) replaced with -
+            def gitlab_username = repo_owner.replaceAll("[^a-zA-Z0-9_-]","-")
+
             def cas_proj_id
             def cas_repo_id
             
-            //assuming usernames for now are a concatenation of the user's email, check platform_usermanagement index.js
-            if(repo_owner.contains("@")){
-                git_username = repo_owner.substring(0, owner.indexOf("@"))
-            }
-
-            def user_id = getGitlabUserId(git_username, ${scm_config.SCM.PRIVATE_TOKEN})
+            def user_id = getGitlabUserId(gitlab_username)
             
             def gitlab_repo_output = sh (
                 script: "curl --header \"Private-Token: ${scm_config.SCM.PRIVATE_TOKEN}\" -X POST \"http://${scm_config.REPOSITORY.BASE_URL}/api/v3/projects/user/$user_id?name=$repo_name&path=$repo_name&visibility=private&request_access_enabled=true\"",
@@ -76,7 +72,7 @@ def createProject(repo_owner, repo_name){
         }
     }catch (ex) {
         if(!((ex.getMessage()).indexOf("groovy.json.internal.LazyMap") > -1)) {
-            echo "createUserInSCM Failed"
+            echo "createProject Failed"
             error "createProjectInSCM Failed. "+ex.getMessage()
         }
     }
@@ -85,7 +81,7 @@ def createProject(repo_owner, repo_name){
 def getGitlabUserId(gitlab_username){
     try{
         def output = sh (
-            script: "curl --header \"Private-Token: ${scm_config..SCM.PRIVATE_TOKEN}\" -X GET \"http://${scm_config.REPOSITORY.BASE_URL}/api/v3/users?username=$gitlab_username\"",
+            script: "curl --header \"Private-Token: ${scm_config.SCM.PRIVATE_TOKEN}\" -X GET \"http://${scm_config.REPOSITORY.BASE_URL}/api/v3/users?username=$gitlab_username\"",
             returnStdout: true
         ).trim()
   
@@ -183,6 +179,7 @@ def addWebhook(repo_name, webhookName, targetUrl) {
         // gitlabs has no support to name a webhook
         sh "curl --header \"Private-Token: ${scm_config.SCM.PRIVATE_TOKEN}\" -X POST \"http://${scm_config.REPOSITORY.BASE_URL}/api/v3/projects/$proj_id/hooks?enable_ssl_verification=false&push_events=true&url=$targetUrl\""
     }else if(scm_config.SCM.TYPE == "bitbucket"){
+        def scm_webhook_api = "${scm_protocol}${scm_config.REPOSITORY.BASE_URL}/rest/webhook/1.0/projects/${scm_config.REPOSITORY.REPO_BASE_SERVICES}/repos/" 
         sh "curl -X PUT -k -v -u \"${scm_config.SCM.USERNAME}:${scm_config.SCM.PASSWORD}\" -H \"Content-Type: application/json\" ${scm_webhook_api}${repo_name}/configurations  -d \'{\"title\": \"${webhookName}\", \"url\": \"${targetUrl}\" , \"enabled\": true}\'"
     }
 }
