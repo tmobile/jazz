66f934e4ff06318acb96f315121a765dbe58b3e3
SiniWilson <Sini.Wilson@ust-global.com>
2018-04-02 09:25:00 +0530

    In cooperating review comments

--- jazz_services-handler/index.js
+++ jazz_services-handler/index.js
@@ -1,25 +1,3 @@
-// =========================================================================
-// Copyright © 2017 T-Mobile USA, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// =========================================================================
-
-/*jshint loopfunc:true */
-/**
-This Handler looks for Service Creation events and updates Service Catalog
-@version: 1.0
- **/
-
 const config = require('./components/config.js'); //Import the environment data.
 const logger = require("./components/logger.js"); //Import the logging module.
 const async = require("async");
@@ -51,8 +29,9 @@ var handler = (event, context, cb) => {
 			return cb(null, records);
 		})
 		.catch(err => {
+			var records = getEventProcessStatus();
 			logger.error("Error processing events. " + JSON.stringify(err));
-			return cb(err);
+			return cb(null, records);
 		});
 
 };
@@ -111,8 +90,6 @@ var processRecord = function (record, configData,authToken) {
 				payload = result.payload;
 				if (result.interested_event) {
 					return processEvent(payload, configData, authToken);
-				} else {
-					handleProcessedEvents(sequenceNumber, payload);
 				}
 			})
 			.then(result => {
