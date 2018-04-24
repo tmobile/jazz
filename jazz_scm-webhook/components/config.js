b06dab38fd0a5cc20abf376b91b859a593057ed6
Suprita S T <suprita@moonraft.com>
2018-04-25 00:21:02 +0530

    worked on review comments

--- jazz_scm-webhook/components/config.js
+++ jazz_scm-webhook/components/config.js
@@ -23,19 +23,10 @@
 **/
 
 var getStageConfig = (event) => {
-  var stage = event.stage;
-  var configObj = {};
-  // Loads the config files based on the env.
-  // Please edit the JSON files.
-  if (stage === 'dev') {
-    configObj = require('../config/dev-config.json');
-  } else if (stage === 'stg') {
-    configObj = require('../config/stg-config.json');
-  } else if (stage === 'prod') {
-    configObj = require('../config/prod-config.json');
-  } else {
-    configObj = require('../config/' + stage + '-config.json');
-  }
+  var stage = event.stage,
+  stageMap = { 'dev': 'dev', 'stg': 'stg', 'prod': 'prod', stage:stage},
+  stageConfig = stageMap[stage] ? stageMap[stage] : stage,
+  configObj = require('../config/'+stageConfig+'-config.json');
 
   return configObj;
 };
