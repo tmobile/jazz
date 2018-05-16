1ee2770f3ac24a743b6780c199ca6dc5f82f4948
Michael Chinn <chnn.michael@gmail.com>
2018-05-16 02:15:58 -0700

    reset env

--- jazz-ui/src/environments/environment.oss.ts
+++ jazz-ui/src/environments/environment.oss.ts
@@ -1,16 +1,14 @@
-// export const environment = {
-//   production:true,
-//   configFile: 'config/config.oss.json',
-//   baseurl: "https://a32wzpxs8i.execute-api.us-east-1.amazonaws.com/prod",
-//   api_doc_name : "jazz20180516-jazz-s3-api-doc-20180516063837987400000010",
-//   envName:"oss",
-//   multi_env:"{multi_env}",
-//   serviceTabs:["overview","logs"],
-//   environmentTabs:["overview"],
-//   urls:{
-//     docs_link:"https://github.com/tmobile/jazz/wiki",
-// }
-//
-// };
+export const environment = {
+  production:true,
+  configFile: 'config/config.oss.json',
+  baseurl: "https://{API_GATEWAY_KEY_PROD}.execute-api.{inst_region}.amazonaws.com/prod",
+  api_doc_name : "https://{api_doc_name}.s3.amazonaws.com",
+  envName:"oss",
+  multi_env:{multi_env},
+  serviceTabs:["overview","logs"],
+  environmentTabs:["overview"],
+  urls:{
+    docs_link:"https://github.com/tmobile/jazz/wiki",
+  }
 
-
+};
