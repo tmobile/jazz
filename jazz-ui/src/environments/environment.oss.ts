f96ab2fdc7f9c6ee69bcb1f25f9656422b5faf24
Michael Chinn <chnn.michael@gmail.com>
2018-05-17 12:40:32 -0700

    env

--- jazz-ui/src/environments/environment.oss.ts
+++ jazz-ui/src/environments/environment.oss.ts
@@ -4,7 +4,7 @@ export const environment = {
   baseurl: "https://7in2k6h272.execute-api.us-east-1.amazonaws.com/prod",
   api_doc_name : "https://{api_doc_name}.s3.amazonaws.com",
   envName:"oss",
-  multi_env:"{multi_env}",
+  multi_env: true,
   serviceTabs:["{overview}","{access control}","{metrics}","{logs}","{cost}"],
   environmentTabs:["{env_overview}","{deployments}","{code quality}","{assets}","{env_logs}"],
   urls:{
