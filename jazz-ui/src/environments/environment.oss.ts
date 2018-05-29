3856bfaf40a757e4b932f654395a7e1c5fc61861
Michael Chinn <chnn.michael@gmail.com>
2018-05-28 23:13:13 -0700

    reset content base

--- jazz-ui/src/environments/environment.oss.ts
+++ jazz-ui/src/environments/environment.oss.ts
@@ -9,7 +9,7 @@ export const environment = {
   environmentTabs:["overview"],
   urls:{
     docs_link:"https://github.com/tmobile/jazz/wiki",
-    content_base: "https://raw.githubusercontent/tmobile/jazz/jazz-content/jazz-ui/"
+    content_base: "https://raw.githubusercontent.com/tmobile/jazz-content/master"
 }
 
 };
