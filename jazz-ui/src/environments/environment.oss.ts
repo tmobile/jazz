e23ebd85e864ad445b93e75fa8c4697dab5b6dcf
Michael Chinn <chnn.michael@gmail.com>
2018-05-30 14:07:40 -0700

    added content_base

--- jazz-ui/src/environments/environment.oss.ts
+++ jazz-ui/src/environments/environment.oss.ts
@@ -4,11 +4,12 @@ export const environment = {
   baseurl: "https://{API_GATEWAY_KEY_PROD}.execute-api.{inst_region}.amazonaws.com/prod",
   api_doc_name : "https://{api_doc_name}.s3.amazonaws.com",
   envName:"oss",
-  multi_env:{multi_env},
+  multi_env:"{multi_env}",
   serviceTabs:["{overview}","{access control}","{metrics}","{logs}","{cost}"],
   environmentTabs:["{env_overview}","{deployments}","{code quality}","{assets}","{env_logs}"],
   urls:{
     docs_link:"https://github.com/tmobile/jazz/wiki",
+    content_base: "https://github.com/tmobile/jazz-content/blob/master"
 }
 
 };
