e2bfd1defd03c792f1da94715bf9ac9315b91da3
Sajjad, Asif <Asif.Sajjad2@T-Mobile.com>
2018-06-01 11:02:05 -0500

    Added test code

---
+++ api-template-java/src/test/java/com/slf/util/EnvironmentConfigTest.java
@@ -0,0 +1,44 @@
+package com.slf.util;
+
+
+import com.slf.model.Request;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author: Asif Sajjad
+ *
+ */
+public class EnvironmentConfigTest {
+
+
+    @Test
+    public void getConfig() throws Exception{
+        Request request = new Request();
+        request.setStage("DEV");
+        EnvironmentConfig config = new EnvironmentConfig(request);
+        String key = config.getConfig("config_key");
+        assertEquals("config_value_dev", key);
+
+    }
+
+    @Test
+    public void getConfigThrowsExceptionWhenNUllStagePassed() {
+        Request request = new Request();
+        request.setStage(null);
+        EnvironmentConfig config = null;
+
+        try {
+            config = new EnvironmentConfig(request);
+            assertTrue("This method should have thrown BadResquestException",false);
+        }
+        catch (Exception e){
+            assertTrue(true);
+        }
+    }
+
+
+
+}
