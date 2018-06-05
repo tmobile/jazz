c5cb5cd33a81ca788b44c56f499191f9469a7dd8
Michael Chinn <chnn.michael@gmail.com>
2018-06-05 16:57:38 -0700

    env reset

--- jazz-ui/src/environments/environment.oss.ts
+++ jazz-ui/src/environments/environment.oss.ts
@@ -2,7 +2,7 @@ export const environment = {
   production:true,
   configFile: 'config/config.oss.json',
   baseurl: "https://{API_GATEWAY_KEY_PROD}.execute-api.{inst_region}.amazonaws.com/prod",
-  api_doc_name : "https://jazz20180601-jazz-s3-api-doc-20180601112438813900000010.s3.amazonaws.com",
+  api_doc_name : "https://{api_doc_name}.s3.amazonaws.com",
   envName:"oss",
   multi_env:"{multi_env}",
   serviceTabs:["{overview}","{access control}","{metrics}","{logs}","{cost}"],
