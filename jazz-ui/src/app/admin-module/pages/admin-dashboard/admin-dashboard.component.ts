c0112ffaebc4ed0934e7bc648317d86f491ca319
Michael Chinn <chnn.michael@gmail.com>
2018-06-07 17:19:43 -0700

    rebase manual

---
+++ jazz-ui/src/app/admin-module/pages/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,39 @@
+import {AfterViewChecked, AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {AdminUtilsService} from "../../services/admin-utils.service";
+
+@Component({
+  selector: 'admin-dashboard',
+  templateUrl: './admin-dashboard.component.html',
+  styleUrls: ['./admin-dashboard.component.scss']
+})
+export class AdminDashboardComponent implements OnInit{
+
+  @ViewChild('jsonRoot') jsonRoot;
+  public adminData;
+  public isCollapsed = true;
+  public state;
+
+  constructor(private adminUtils: AdminUtilsService) {
+  }
+
+  ngOnInit() {
+    this.state = 'loading';
+    this.adminUtils.getExampleVars()
+      .then((data: any) => {
+        this.state = 'resolved';
+        this.adminData = data;
+      }, (error) => {
+        this.state = 'error'
+      })
+  }
+
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
+    this.jsonRoot.setCollapse(this.isCollapsed);
+  }
+
+  reportIssue() {
+
+  }
+
+}
