7689ea1b8ff34c3a3c372dcc83bc5b5c20d4e7f2
Michael Chinn <chnn.michael@gmail.com>
2018-06-11 09:05:39 -0700

    header reset

--- jazz-ui/src/app/secondary-components/jazz-header/jazz-header.component.ts
+++ jazz-ui/src/app/secondary-components/jazz-header/jazz-header.component.ts
@@ -51,7 +51,7 @@ export class JazzHeaderComponent implements OnInit {
     buttonText:string='SUBMIT';
     loggedinUser:string='';
     overridingUrl:string = "";
-    public isAdmin: false;
+    public isAdmin = false;
 
 
     public toggleLoginPanel () {
@@ -126,7 +126,6 @@ docs_oss_jazz:string=env_oss.urls.docs_link;
               this.loading = false;
             //   let errorMessage  = this.toastmessage.errorMessage(error,"logout");
             //   this.toasterService.pop('error', 'Oops!', errorMessage);
-
                //for trmporary period till demo(21 aug '17)
                this.isLoggedIn = this.authenticationService.isLoggedIn();
                this.router.navigateByUrl('');// Route to landing page
@@ -256,8 +255,9 @@ docs_oss_jazz:string=env_oss.urls.docs_link;
 
         this.checkUrl();
         this.isLoggedIn = this.authenticationService.isLoggedIn();
-            this.loggedinUser =this.isLoggedIn && this.authenticationService.getUserId();
-            this.isAdmin = this.isLoggedIn && this.authenticationService.getAuthenticatedUser().globaladmin
+        this.isLoggedIn = this.authenticationService.isLoggedIn();
+        this.loggedinUser =this.isLoggedIn && this.authenticationService.getUserId();
+        this.isAdmin = this.isLoggedIn && this.authenticationService.getAuthenticatedUser().globaladmin
     }
 }
 
