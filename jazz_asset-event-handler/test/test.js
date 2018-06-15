ee6b4edabb7584922958c410db0e1c228349a26a
Michael Chinn <chnn.michael@gmail.com>
2018-06-15 15:07:43 -0700

    Revert "tests"
    This reverts commit 7ecf411c0305ecdd671cbec724783bceb5a095dc.

--- jazz_asset-event-handler/test/test.js
+++ jazz_asset-event-handler/test/test.js
@@ -13,147 +13,57 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // =========================================================================
-const _ = require('lodash');
+
 const chai = require('chai');
-const assert = chai.assert;
-const expect = chai.expect;
-const chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
-chai.should();
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const should = require('chai').should();
+const chaiAsPromised = require('chai-as-promised'); chai.use(chaiAsPromised);
+const request = require('request');
+const AWS = require('aws-sdk-mock');
 const awsContext = require('aws-lambda-mock-context');
 const sinon = require('sinon')
-const sinonTest = require('sinon-test')(sinon, {useFakeTimers: false});
+const sinonTest = require('sinon-test')(sinon, { useFakeTimers: false });
 require('sinon-as-promised');
-const request = require('request');
 const rp = require('request-promise-native');
 const index = require('../index');
 const logger = require('../components/logger');
 const config = require('../components/config');
-const mockData = require('./mock-data/index');
+var testPayloads = require('./response_payloads.js')();
+var kinesisPayload = require('./KINESIS_PAYLOAD');
 
-var event, context, configData, authToken, sandbox;
+var event, context, configData, authToken
 
 describe('jazz asset handler tests: ', function () {
-  before(function () {
-    sandbox = sinon.sandbox.create();
-  });
 
+  var sandbox;
   beforeEach(function () {
-    sandbox.stub(logger);
+    sandbox = sinon.sandbox.create();
     context = awsContext();
-    context.functionName = context.functionName + "-test";
+    context.functionName = context.functionName + "-test"
     configData = config(context);
-    authToken = mockData.tokenResponseObj200.body.data.token
+    authToken = testPayloads.tokenResponseObj200.body.data.token
+
   });
 
   afterEach(function () {
     sandbox.restore();
   });
 
+  it('Verified getToken returned a valid 200 response ', function () {
+    var requestPromoiseStub = sinon.stub(rp, 'Request').returns(Promise.resolve(testPayloads.tokenResponseObj200));
+    var getTokenRequest = index.getTokenRequest(configData);
 
-  describe.only('processItem', function() {
-
-    beforeEach(function () {
-        this.payload = _.cloneDeep(mockData.eventPayload);
-    });
-
-    // it('verify creates asset on create asset event', function () {
-    //   this.payload.EVENT_STATUS.S = 'CREATE_EVENT';
-    //   sinon.stub(index.processUpdateAsset).resolves();
-    // });
-
-
-    it('verify create asset updates if exists', function () {
-      this.payload.EVENT_NAME.S = 'CREATE_ASSET';
-      sinon.stub(index, 'checkIfAssetExists').rejects();
-      sinon.stub(index, 'processCreateAsset').resolves();
-
-      var response = index.processItem(this.payload, configData, authToken);
-
-      sinon.assert.called(index.processCreateAsset);
-      // sinon.assert.called(logger.error);
-      // return expect(response).to.eventually.be.rejected;
-    });
-
-  });
-
-
-
-  describe('responds to successful http', function () {
-    beforeEach(function () {
-      sandbox.stub(request, 'Request', function (options) {
-        options.callback.apply(this, mockData.requestSuccessCallback);
+    var verified = rp(getTokenRequest)
+      .then(res => {
+        var status = res.statusCode;
+        expect(status, "Invalid status Code from getToken").to.eql(200);
       });
-    });
 
-    it('Verify CREATE ASSET successful', function () {
-      var response = index.processCreateAsset(mockData.eventPayload, configData, authToken);
-
-      return expect(response).to.eventually.be.resolved;
-    });
-
-    it('Verify UPDATE ASSET successful', function () {
-      var response = index.processUpdateAsset(mockData.recordsPayload.Records[0], mockData.eventPayload, configData, authToken);
-
-      return expect(response).to.eventually.be.resolved;
-    });
-
-    it('Verify CheckIfAssetExits asset exists', function () {
-      var response = index. checkIfAssetExists(mockData.eventPayload, configData, authToken);
-
-      return expect(response).to.eventually.be.resolved;
-    });
-
+    requestPromoiseStub.restore();
+    return verified;
   });
 
-  describe('responds to unsuccessful http', function() {
-    beforeEach(function () {
-      sandbox.stub(request, 'Request', function (options) {
-        options.callback.apply(this, mockData.requestInternalErrorCallback);
-      });
-    });
+  // More Test cases to be added. 
 
-    it('Verify CREATE ASSET unsuccessful', function () {
-      var response = index.processCreateAsset(mockData.eventPayload, configData, authToken);
-
-      sinon.assert.called(logger.error);
-      return expect(response).to.eventually.be.rejected;
-    });
-
-    it('Verify UPDATE ASSET successful', function () {
-      var response = index.processUpdateAsset(mockData.recordsPayload.Records[0], mockData.eventPayload, configData, authToken);
-
-      sinon.assert.called(logger.error);
-      return expect(response).to.eventually.be.rejected;
-    });
-
-    it('Verify CheckIfAssetExits asset exists', function () {
-      var response = index. checkIfAssetExists(mockData.eventPayload, configData, authToken);
-
-      sinon.assert.called(logger.error);
-      return expect(response).to.eventually.be.rejected;
-    });
-
-  });
-
-  describe('incorrect event status', function() {
-    beforeEach(function () {
-      this.payload = _.cloneDeep(mockData.eventPayload);
-      this.payload.EVENT_STATUS.S = 'NON_RELEVENT_STATUS';
-    });
-
-    it('verify create asset fails', function () {
-      var response = index.processCreateAsset(this.payload, configData, authToken);
-
-      return expect(response).to.eventually.be.rejected;
-    });
-
-    it('verify create asset fails', function () {
-      var response = index.processUpdateAsset(mockData.recordsPayload.Records[0], this.payload, configData, authToken);
-
-      return expect(response).to.eventually.be.rejected;
-    });
-
-  });
-  
 });
