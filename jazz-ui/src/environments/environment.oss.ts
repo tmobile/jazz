14a099a7aefd39e1bb0018180f16a3db4dfbed42
Sughosh R J <sughosh@moonraft.com>
2018-06-21 23:05:02 +0530

    no message

--- jazz-ui/src/environments/environment.oss.ts
+++ jazz-ui/src/environments/environment.oss.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production:true,
   configFile: 'config/config.oss.json',
-  baseurl: "https://v7m8a1nr30.execute-api.us-east-1.amazonaws.com/prod",
+  baseurl: "https://{API_GATEWAY_KEY_PROD}.execute-api.{inst_region}.amazonaws.com/prod",
   api_doc_name : "https://{api_doc_name}.s3.amazonaws.com",
   envName:"oss",
-  multi_env:true,
-  serviceTabs:["overview","access control","metrics","logs","cost"],
-  environmentTabs:["env_overview","deployments","code quality","assets","env_logs"],
+  multi_env:{multi_env},
+  serviceTabs:["{overview}","{access control}","{metrics}","{logs}","{cost}"],
+  environmentTabs:["{env_overview}","{deployments}","{code quality}","{assets}","{env_logs}"],
   urls:{
     docs_link:"https://github.com/tmobile/jazz/wiki",
-},
+  },
   userJourney: {
     registrationMessage: 'Please contact your Jazz Admin(s) to get a registration code.'
   }
