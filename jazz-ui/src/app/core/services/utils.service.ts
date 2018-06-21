7038b4ec3f75c4ec19f6811c85964acfd31944e4
Michael Chinn <chnn.michael@gmail.com>
2018-06-21 02:37:54 -0700

    first

---
+++ jazz-ui/src/app/core/services/utils.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class UtilsService {
+
+  constructor() {
+  }
+
+  debounce(func, wait, immediate) {
+    let timeout;
+    return function () {
+      const context = this, args = arguments;
+      const later = function () {
+        timeout = null;
+        if (!immediate){
+          func.apply(context, args);
+        }
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) {
+        func.apply(context, args);
+      }
+    };
+  };
+}
