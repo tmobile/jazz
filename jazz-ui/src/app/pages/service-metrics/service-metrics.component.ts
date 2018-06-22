cf6484bb4447714934e436cf2eaa658799ffd3fe
Sughosh R J <sughosh@moonraft.com>
2018-06-22 20:27:06 +0530

    no message

--- jazz-ui/src/app/pages/service-metrics/service-metrics.component.ts
+++ jazz-ui/src/app/pages/service-metrics/service-metrics.component.ts
@@ -101,7 +101,7 @@ export class ServiceMetricsComponent implements OnInit {
   envSelected:string = this.envList[0];
 
   statisticList: Array<string> = ['Average', 'Sum', 'Maximum','Minimum'];
-  statisticSelected:string= this.statisticList[0];
+  statisticSelected:string= this.statisticList[1];
   viewBox = "0 0 300 150";
   today = new Date();
   yesterday = this.today.setDate(this.today.getDate()-1);
