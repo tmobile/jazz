df0ba6e02413656763be179b640a4b82333e200b
Hemanth KS <suprita@moonraft.com>
2018-06-25 18:24:58 +0530

    Added Admin API -

---
+++ jazz_admin-api/test/test.js
@@ -0,0 +1,11 @@
+const assert = require('chai').assert;
+const index = require('../index');
+
+describe('Sample', function() {
+  it('tests handler', function(done) {
+
+    // Add your test cases here.
+    assert(true);
+    done();
+  });
+});
