c0339b0cf0e5c984c768eabb2c4400aaf1d6807b
Michael Chinn <chnn.michael@gmail.com>
2018-06-26 13:50:42 -0700

    conditionalize environment

--- jazz-ui/src/environments/environment.oss.ts
+++ jazz-ui/src/environments/environment.oss.ts
@@ -7,6 +7,9 @@ export const environment = {
   multi_env:{multi_env},
   serviceTabs:["{overview}","{access control}","{metrics}","{logs}","{cost}"],
   environmentTabs:["{env_overview}","{deployments}","{code quality}","{assets}","{env_logs}"],
+  createService: {
+    deploymentTargets: {deployment_targets}
+  },
   urls:{
     docs_link:"https://github.com/tmobile/jazz/wiki",
 }
