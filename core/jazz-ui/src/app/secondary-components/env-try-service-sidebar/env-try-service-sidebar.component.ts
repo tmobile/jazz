e006188911f1cb316df8ec5ca7073e720df3935a
Michael Chinn <chnn.michael@gmail.com>
2018-06-29 16:40:09 -0700

    test lambda

--- core/jazz-ui/src/app/secondary-components/env-try-service-sidebar/env-try-service-sidebar.component.ts
+++ core/jazz-ui/src/app/secondary-components/env-try-service-sidebar/env-try-service-sidebar.component.ts
@@ -1,12 +1,12 @@
 import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
 import {SessionStorageService} from "../../core/helpers/session-storage.service";
 import {RelaxedJsonService} from "../../core/helpers/relaxed-json.service";
-import { HttpModule } from '@angular/http';
-import { RequestService } from "../../core/services";
-
+import {HttpModule} from '@angular/http';
+import {RequestService} from "../../core/services";
 
 
 declare var Promise;
+
 @Component({
   selector: 'env-try-service-sidebar',
   templateUrl: './env-try-service-sidebar.component.html',
@@ -18,18 +18,18 @@ export class EnvTryServiceSidebarComponent implements OnInit {
   public contentTypeMenu = ['application/json'];
   public contentTypeSelected = this.contentTypeMenu[0];
   public lineNumberCount: any = new Array(5).fill('');
-  public lineNumberCount_op  : any = new Array(5).fill('');
+  public lineNumberCount_op: any = new Array(5).fill('');
   public inputValue = '';
   public outputValue = '';
+  public outputHeader = null;
   public valid = true;
   public validityMessage = '';
   public savingPayload = true;
   public sessionStorageKey = 'jazz-function-payload';
   public loading;
-  private subscription:any;
-  private http:any;
-  api_status:string="default";
-
+  private subscription: any;
+  private http: any;
+  api_status: string = "default";
 
 
   @Input() service;
@@ -38,66 +38,44 @@ export class EnvTryServiceSidebarComponent implements OnInit {
 
   constructor(private sessionStorage: SessionStorageService,
               private relaxedJson: RelaxedJsonService,
-              private request:RequestService,
-            ) {
-              this.http = request;
+              private request: RequestService,
+  ) {
+    this.http = request;
 
   }
 
   ngOnInit() {
     let saved = this.sessionStorage.getItem(this.sessionStorageKey);
+    this.savingPayload = !!saved;
     if (saved) {
       this.inputValue = saved;
     }
   }
 
 
- 
   startTest() {
     this.inputIsValid();
     if (this.valid) {
       this.loading = true;
-
-      var promise = new Promise((resolve, reject) => {
-        var payload = {} ;
-        let outputObject = {};
-        console.log('serv',this.environment);
-        payload["functionARN"]= this.environment.endpoint;
-        // "arn:aws:lambda:us-east-1:192006145812:function:gitlab180627-jazztest-lambda-prod:1";
-        payload["inputJSON"] = JSON.parse(this.inputValue);
-        this.subscription = this.http.post('https://p1cgv1qy3c.execute-api.us-east-1.amazonaws.com/prod/jazz/test-lambda', payload).subscribe(
-        (response) => {        
-          response.data.execStatus = "error";
-          if(response.data.execStatus == "Success"){
-            this.api_status = "success";
-          }            
-          else this.api_status = "error";
-
-          console.log('response =>',response);
-          console.log('ss',response.data.payload.Payload);
-          console.log('sss',response.data.payload.StatusCode);
-          outputObject["data"]=JSON.parse(response.data.payload.Payload);
-          outputObject["status"]=response.data.payload.StatusCode;
-          setTimeout(() => { 
-            resolve(outputObject);
-          }, 500)
-
-
-        },
-        (error) => {
-          this.api_status = "error";
-          console.log('error =>',error);
-    
-        
-        })
-      });
-
-      promise.then((response) => {
+      let payload = {
+        "functionARN": this.environment.endpoint,
+        "inputJSON": this.inputValue
+      };
+      this.subscription = this.http.post('/jazz/test-lambda', payload).subscribe((response) => {
         this.loading = false;
-        this.outputValue = this.stringToPrettyString(JSON.stringify(response));
+        this.outputHeader = {
+          statusCode: response.data.payload.StatusCode,
+          statusText: response.data.execStatus
+        }
+        this.outputValue = this.stringToPrettyString(response.data.payload.Payload);
         this.lineNumbers('op');
-
-        console.log('this.out',this.outputValue)
+      }, (error) => {
+        this.loading = false;
+        this.outputHeader = {
+          statusCode: error.status,
+          statusText: error.statusText || 'Error'
+        };
+        this.outputValue = 'Error'
       });
     }
   }
@@ -111,22 +89,22 @@ export class EnvTryServiceSidebarComponent implements OnInit {
     }
   }
 
-  lineNumbers(event){
+  lineNumbers(event) {
     var lines;
-    if(event == "op"){
+    if (event == "op") {
       lines = this.outputValue.split(/\r*\n/);
     }
-    else{
+    else {
       lines = this.inputValue.split(/\r*\n/);
-    } 
+    }
     var line_numbers = lines.length;
-    if(event == "op"){
+    if (event == "op") {
       this.lineNumberCount_op = new Array(line_numbers).fill('');
     }
-    else{
+    else {
       this.lineNumberCount = new Array(line_numbers).fill('');
-    }    
-    
+    }
+
   }
 
   toggleSavePayload(flag) {
@@ -144,9 +122,10 @@ export class EnvTryServiceSidebarComponent implements OnInit {
       this.valid = true;
       this.toggleSavePayload(this.savingPayload);
       this.inputValue = jsonString;
+      this.toggleSavePayload(this.savingPayload);
       this.lineNumbers('ip');
     } catch (error) {
-      console.log('parse error',error);
+      console.log('parse error', error);
       this.validityMessage = 'Could not format JSON';
       this.valid = false;
     }
@@ -170,6 +149,7 @@ export class EnvTryServiceSidebarComponent implements OnInit {
 
   closeSideMenu() {
     this.outputValue = '';
+    this.outputHeader = null;
     this.onClose.emit();
   }
 
--- core/jazz-ui/src/environments/environment.oss.ts
+++ core/jazz-ui/src/environments/environment.oss.ts
@@ -1,17 +1,16 @@
 export const environment = {
-  production:true,
+  production: true,
   configFile: 'config/config.oss.json',
-  baseurl: "https://p1cgv1qy3c.execute-api.us-east-1.amazonaws.com/prod",
-  api_doc_name : "https://{api_doc_name}.s3.amazonaws.com",
-  envName:"oss",
-  multi_env:true,
-  serviceTabs:["overview","access control","metrics","logs","cost"],
-  environmentTabs:["overview","deployments","code quality","assets","env_logs"],
-  urls:{
-    docs_link:"https://github.com/tmobile/jazz/wiki",
+  baseurl: "https://{API_GATEWAY_KEY_PROD}.execute-api.{inst_region}.amazonaws.com/prod",
+  api_doc_name: "https://{api_doc_name}.s3.amazonaws.com",
+  envName: "oss",
+  multi_env: {multi_env},
+  serviceTabs: ["{overview}", "{access control}", "{metrics}", "{logs}", "{cost}"],
+  environmentTabs: ["{env_overview}", "{deployments}", "{code quality}", "{assets}", "{env_logs}"],
+  urls: {
+    docs_link: "https://github.com/tmobile/jazz/wiki",
   },
   userJourney: {
     registrationMessage: 'Please contact your Jazz Admin(s) to get a registration code.'
   }
-
 };
