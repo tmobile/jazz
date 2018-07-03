4f5e42df81f1a73870744dd8b4ba70176962f5a8
Hemanth KS <hemanth.ks@moonraft.com>
2018-07-03 12:48:55 +0530

    Updated to new config

--- core/jazz_admin/components/config.js
+++ core/jazz_admin/components/config.js
@@ -1,5 +1,5 @@
 // =========================================================================
-// Copyright ©  2017 T-Mobile USA, Inc.
+// Copyright © 2017 T-Mobile USA, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,32 +15,46 @@
 // =========================================================================
 
 /**
-API for the admin user to retrieve the installer vars JSON
-@author:
-@version: 1.0
- **/
+  Nodejs Template Project
+  @module: config.js
+  @description: Defines variables/functions to retrieve environment related data
+  @author:
+  @version: 1.0
+**/
 
+const fs = require('fs');
+const path = require('path');
 
-var getStageConfig = (event) => {
-  var stage;
+var getStageConfig = (event, context) => {
+  var stage, configObj;
 
-  if (event && event.awslogs && event.awslogs.data) {
-    // cw events default to dev
-    stage = 'dev';
-  } else {
-    stage = event.stage
+  if (event && event.stage) {
+    stage = event.stage;
+  } else if (context && context.functionName && context.functionName.length > 0) {
+    var functionName = context.functionName;
+
+    var fnName = functionName.substr(functionName.lastIndexOf('-') + 1, functionName.length);
+
+    if (fnName.endsWith('dev')) {
+      stage = 'dev';
+    } else if (fnName.endsWith('stg')) {
+      stage = 'stg';
+    } else if (fnName.endsWith('prod')) {
+      stage = 'prod';
+    }
   }
 
-  var configObj;
-
   if (stage) {
-    configObj = require(`../config/${stage}-config.json`);
+    var configFile = path.join(__dirname, `../config/${stage}-config.json`);
+
+    if (fs.existsSync(configFile)) {
+      configObj = JSON.parse(fs.readFileSync(configFile));
+    }
   }
 
   return configObj;
 };
 
-module.exports = (event) => {
-  var config = getStageConfig(event);
-  return config;
-};
+module.exports = {
+  getConfig: getStageConfig
+}
