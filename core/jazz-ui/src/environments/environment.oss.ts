ce2ca832d258b0d33295291275f7436075fac973
Michael Chinn <chnn.michael@gmail.com>
2018-07-11 14:27:26 -0700

    env reset swagger hard code

--- core/jazz-ui/src/environments/environment.oss.ts
+++ core/jazz-ui/src/environments/environment.oss.ts
@@ -4,12 +4,12 @@ export const environment = {
   baseurl: "https://{API_GATEWAY_KEY_PROD}.execute-api.{inst_region}.amazonaws.com/prod",
   api_doc_name : "https://{api_doc_name}.s3.amazonaws.com",
   envName:"oss",
-  multi_env:"{multi_env}",
+  multi_env:{multi_env},
   serviceTabs:["{overview}","{access control}","{metrics}","{logs}","{cost}"],
   environmentTabs:["{env_overview}","{deployments}","{code quality}","{assets}","{env_logs}"],
   urls:{
     docs_link:"https://github.com/tmobile/jazz/wiki",
-    swagger_editor: "{swagger_editor}"
+    swagger_editor: "https://editor.swagger.io"
   },
   userJourney: {
     registrationMessage: 'Please contact your Jazz Admin(s) to get a registration code.'
