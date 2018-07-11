cca1886b45ca8307b852151d98552ea3d5d0b6f1
Michael Chinn <chnn.michael@gmail.com>
2018-07-11 15:40:17 -0700

    metrics

--- core/jazz-ui/src/environments/environment.oss.ts
+++ core/jazz-ui/src/environments/environment.oss.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   configFile: 'config/config.oss.json',
-  baseurl: "https://{API_GATEWAY_KEY_PROD}.execute-api.{inst_region}.amazonaws.com/prod",
+  baseurl: "https://p22i216yc6.execute-api.us-east-1.amazonaws.com/prod",
   api_doc_name : "https://{api_doc_name}.s3.amazonaws.com",
   envName:"oss",
-  multi_env:{multi_env},
+  multi_env:"{multi_env}",
   serviceTabs:["{overview}","{access control}","{metrics}","{logs}","{cost}"],
-  environmentTabs:["{env_overview}","{deployments}","{code quality}", "{metrics}", "{assets}","{env_logs}"],
+  environmentTabs:["{env_overview}","{deployments}","{code quality}", "metrics", "{assets}","{env_logs}"],
   urls:{
     docs_link:"https://github.com/tmobile/jazz/wiki",
     swagger_editor: "{swagger_editor}"
