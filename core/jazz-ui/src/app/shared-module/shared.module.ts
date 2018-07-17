eb48acbcc9a70f81867148fad2e6d7eba87ffd5f
Michael Chinn <chnn.michael@gmail.com>
2018-07-17 12:52:14 -0700

    shared module

--- core/jazz-ui/src/app/shared-module/shared.module.ts
+++ core/jazz-ui/src/app/shared-module/shared.module.ts
@@ -57,6 +57,7 @@ import {ChartjsLinegraphComponent} from "../secondary-components/chartjs-linegra
 import {FilterModalComponent} from "../secondary-components/filter-modal/filter-modal.component";
 import {ServiceMetricsComponent} from "../pages/service-metrics/service-metrics.component";
 import {MetricsCarouselComponent} from "../secondary-components/metrics-carousel/metrics-carousel.component";
+import {RouterModule} from "@angular/router";
 
 let specificComponents:any
 let specificModules: any;
