0085a8b74619282af4e3d053fd0bf035d35452d7
Michael Chinn <chnn.michael@gmail.com>
2018-07-17 17:48:58 -0700

    scroller

--- core/jazz-ui/src/app/landing-module/landing-carousel/landing-carousel.component.ts
+++ core/jazz-ui/src/app/landing-module/landing-carousel/landing-carousel.component.ts
@@ -1,36 +1,75 @@
 import {AfterViewInit, Component, OnInit, ViewChild, ViewChildren} from '@angular/core';
 import {LandingService} from "../landing.service";
 import * as _ from "lodash";
+
 @Component({
   selector: 'landing-carousel',
   templateUrl: './landing-carousel.component.html',
   styleUrls: ['./landing-carousel.component.scss']
 })
-export class LandingCarouselComponent implements OnInit {
-  @ViewChildren('card') cards;
+export class LandingCarouselComponent implements OnInit, AfterViewInit {
+  @ViewChild('scrollContainer') scrollContainer;
+  @ViewChild('scrollFloater') scrollFloater;
   public carouselItems;
-  public translateDistance = 0;
-  public cardWidth;
+  public scrollable = false;
+  public translateIndex = 0;
+  public maxShown;
+  public cardWidth = 220;
+  public cardBuffer = 5;
 
-  constructor(private landingService: LandingService) { }
+  constructor(private landingService: LandingService) {
+  }
 
-  ngOnInit() {
+  ngAfterViewInit() {
     this.landingService.getCarouselData()
       .then((data) => {
-          this.carouselItems = data;
+        this.carouselItems = data;
+        this.setShownCards();
       })
   }
 
+  ngOnInit() {
+
+  }
+
+  setShownCards() {
+    let containerWidth = this.scrollContainer.nativeElement.getBoundingClientRect().width;
+    this.maxShown = Math.floor(containerWidth / (this.cardWidth + 10));
+    if (this.maxShown >= this.carouselItems.length) {
+      this.scrollable = false;
+      this.cardBuffer = 0;
+    } else {
+      this.scrollable = true;
+      let extraSpace = containerWidth - (this.maxShown * this.cardWidth);
+      this.cardBuffer = extraSpace / (2 * this.maxShown);
+    }
+  }
+
+  getOffset() {
+    return this.translateIndex * (this.cardWidth + (this.cardBuffer * 2)) + 'px';
+  }
+
   scrollLeft() {
-    let width = this.cards.first.nativeElement.getBoundingClientRect().width;
-    let lastCard = this.carouselItems[this.carouselItems.length - 1];
-    this.translateDistance += width;
+    this.translateIndex -= 1;
   }
 
   scrollRight() {
-    let width = this.cards.first.nativeElement.getBoundingClientRect().width;
-    this.translateDistance += width;
+    this.translateIndex += 1;
   }
+
+  canScrollLeft() {
+    if(!this.scrollContainer || !this.scrollFloater) return;
+    let flag = this.translateIndex > (this.maxShown - this.carouselItems.length);
+    return flag;
+  }
+
+  canScrollRight() {
+    if(!this.scrollContainer || !this.scrollFloater) return;
+    let flag = this.translateIndex < 0;
+    return flag;
+  }
+
+
 
 
 
