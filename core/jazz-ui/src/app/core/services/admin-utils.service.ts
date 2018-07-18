25f282e398351132e70efbe610ac715df6804e90
Michael Chinn <chnn.michael@gmail.com>
2018-07-18 12:01:46 -0700

    indent

--- core/jazz-ui/src/app/core/services/admin-utils.service.ts
+++ core/jazz-ui/src/app/core/services/admin-utils.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Http} from "@angular/http";
 import {Observable} from "rxjs/Observable";
 import {RequestService} from "./request.service";
@@ -15,7 +15,7 @@ export class AdminUtilsService {
     return this.http.get('/jazz/admin/config')
       .toPromise()
       .then((response) => {
-          if(response && response.data && response.data.config)
+        if (response && response.data && response.data.config)
           return response.data.config;
       })
   }
