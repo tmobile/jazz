2ebd27c3277fa341b8ab39f758fa860abd048a1a
Satish Malireddi <dev.satishm@gmail.com>
2018-08-07 14:46:23 -0700

    Removed unwanted comments

--- jazz_apigee-proxy-aws/components/validation.js
+++ jazz_apigee-proxy-aws/components/validation.js
@@ -12,7 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// =========================================================================const errorHandlerModule = require("./components/error-handler.js");
+// =========================================================================
 
 const errorHandlerModule = require("./error-handler.js");
 
