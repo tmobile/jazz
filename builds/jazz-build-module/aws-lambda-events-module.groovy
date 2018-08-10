36c3033df050f34e80722f58b61a0b308bade289
Administrator <serverless@t-mobile.com>
2018-08-10 09:23:18 +0530

    In cooperating the comments for spaces issue

--- builds/jazz-build-module/aws-lambda-events-module.groovy
+++ builds/jazz-build-module/aws-lambda-events-module.groovy
@@ -8,6 +8,7 @@ import groovy.transform.Field
 */
 
 @Field def config_loader
+@Field def queue_visibility_timeout = 165
 
 def initialize(configLoader){
   config_loader = configLoader
@@ -25,52 +26,52 @@ def checkSQSAndAddLambdaTrigger(queueName, lambdaARN) {
 }
 
 def addLambdaTriggerToSqsQueue(isExists, queue_name, lambdaARN){
-  if(isExists){
+  if (isExists) {
     def isDefined = listEventSourceMapping(queue_name, lambdaARN)
-    if(!isDefined){
+    if (!isDefined) {
       createEventSourceMapping(queue_name, lambdaARN)
     }
-  }else{
+  } else {
     createSqsQueue(queue_name, lambdaARN)
   }
 }
 
 def createSqsQueue(queue_name, lambdaARN){
-  try{
-    sh "aws sqs create-queue --queue-name ${queue_name} --attributes '{\"VisibilityTimeout\": \"165\"}' --profile cloud-api --output json"
+  try {
+    sh "aws sqs create-queue --queue-name ${queue_name} --attributes '{\"VisibilityTimeout\": \"${queue_visibility_timeout}\"}' --profile cloud-api --output json"
     createEventSourceMapping(queue_name, lambdaARN)
-  }catch(ex){
+  } catch (ex) {
     echo "Failed to create the queue"
     error "Failed to create the queue"
   }
 }
 
 def createEventSourceMapping(queue_name, arn){
-  try{
+  try {
     def lambdaARN = arn.split(":(?!.*:.*)")[0];
     def queue_arn = "arn:aws:sqs:${config_loader.AWS.REGION}:${config_loader.AWS.ACCOUNTID}:${queue_name}"
     sh "aws lambda create-event-source-mapping --function-name ${lambdaARN} --event-source ${queue_arn} --profile cloud-api --output json"
-  }catch(ex){
+  } catch (ex) {
     echo "Failed to create the event source mapping"
     error "Failed to create the event source mapping"
   }
 }
 
 def listEventSourceMapping(queue_name, arn){
-  try{
+  try {
     def lambdaARN = arn.split(":(?!.*:.*)")[0];
     def queue_arn = "arn:aws:sqs:${config_loader.AWS.REGION}:${config_loader.AWS.ACCOUNTID}:${queue_name}"
-    def response =  sh(
-			script: "aws lambda list-event-source-mappings --function-name ${lambdaARN} --event-source ${queue_arn} --profile cloud-api --output json",
-			returnStdout: true
-		).trim()
+    def response = sh(
+      script: "aws lambda list-event-source-mappings --function-name ${lambdaARN} --event-source ${queue_arn} --profile cloud-api --output json",
+      returnStdout: true
+    ).trim()
     def mappings = parseJson(response)
-    if(mappings.EventSourceMappings.size() > 0){
+    if (mappings.EventSourceMappings.size() > 0) {
       return true
-    }else{
+    } else {
       return false
     }
-  }catch(ex){
+  } catch (ex) {
     echo "Failed to list the event source mapping"
     error "Failed to list the event source mapping"
   }
@@ -78,10 +79,10 @@ def listEventSourceMapping(queue_name, arn){
 
 @NonCPS
 def parseJson(jsonString) {
-    def lazyMap = new groovy.json.JsonSlurper().parseText(jsonString)
-    def m = [:]
-    m.putAll(lazyMap)
-    return m
+  def lazyMap = new groovy.json.JsonSlurper().parseText(jsonString)
+  def m = [:]
+  m.putAll(lazyMap)
+  return m
 }
 
 return this
