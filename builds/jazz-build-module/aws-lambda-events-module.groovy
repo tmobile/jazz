ebcbe512b3168ce478bb2116f8cde4c69c839335
Suprita S T <suprita@moonraft.com>
2018-08-28 19:53:42 +0530

    Delete manually created assets and update assets catalog

--- builds/jazz-build-module/aws-lambda-events-module.groovy
+++ builds/jazz-build-module/aws-lambda-events-module.groovy
@@ -292,29 +292,49 @@ def checkAndConvertEvents(events){
 
 def getStreamEnabledArn(tableStreamArn) {
   def tableName = tableStreamArn.tokenize("/").last()
-  def streamList = sh(
-    script: "aws dynamodbstreams list-streams --table-name ${tableName} --region ${config_loader.AWS.REGION} --output json",
-    returnStdout: true
-  ).trim()
-  def streamListJson = parseJson(streamList)
+  try {
+    sh "aws dynamodb describe-table --table-name ${tableName} --region ${config_loader.AWS.REGION} --output json"
+    echo "table exist..."
+    def streamList = sh(
+      script: "aws dynamodbstreams list-streams --table-name ${tableName} --region ${config_loader.AWS.REGION} --output json",
+      returnStdout: true
+    ).trim()
+    def streamListJson = parseJson(streamList)
 
-  if(streamListJson.Streams.size() == 0) {
-    return createDynamodbStream(tableName)
-  } else {
-    def streamArnList = streamListJson.Streams
-    for (stream in streamArnList) {
-      def streamDetails = sh(
-        script: "aws dynamodbstreams describe-stream --stream-arn ${stream.StreamArn} --region ${config_loader.AWS.REGION} --output json",
-        returnStdout: true
-      ).trim()
-      def streamDetailsJson = parseJson(streamDetails)
+    if (streamListJson.Streams.size() == 0) {
+      return createDynamodbStream(tableName)
+    } else {
+      def streamArnList = streamListJson.Streams
+      for (stream in streamArnList) {
+        def streamDetails = sh(
+          script: "aws dynamodbstreams describe-stream --stream-arn ${stream.StreamArn} --region ${config_loader.AWS.REGION} --output json",
+          returnStdout: true
+        ).trim()
+        def streamDetailsJson = parseJson(streamDetails)
 
-      if ((streamDetailsJson.StreamDescription.StreamStatus == "ENABLED") || (streamDetailsJson.StreamDescription.StreamStatus == "ENABLING")) {
-        return stream.StreamArn
-      } else if (streamArnList.last().StreamArn == stream.StreamArn){
-        return createDynamodbStream(tableName)
+        if ((streamDetailsJson.StreamDescription.StreamStatus == "ENABLED") || (streamDetailsJson.StreamDescription.StreamStatus == "ENABLING")) {
+          return stream.StreamArn
+        } else if (streamArnList.last().StreamArn == stream.StreamArn) {
+          return createDynamodbStream(tableName)
+        }
       }
     }
+  } catch (ex) {
+      def response
+      try {
+        response = sh(
+          script: "aws dynamodb describe-table --table-name ${tableName} --region ${config_loader.AWS.REGION} --output json 2<&1 | grep -c 'ResourceNotFoundException'",
+          returnStdout: true
+        ).trim()
+      } catch (e) {
+        echo "Error occured while describing the dynamodb"
+      }
+      if (response) {
+        echo "Dynamodb does not exists"
+        return false
+      } else {
+        error "Error occured while describing the dynamodb details"
+      }
   }
 }
 
