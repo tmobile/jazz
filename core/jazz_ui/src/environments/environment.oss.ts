c2a2c6fe0336596b17ad4e90429ee76be98c2f9d
Sughosh R J <sughosh@moonraft.com>
2018-08-30 18:15:33 +0530

    no message

--- core/jazz_ui/src/environments/environment.oss.ts
+++ core/jazz_ui/src/environments/environment.oss.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   configFile: 'config/config.oss.json',
-  baseurl: "https://wkbk1dhrd9.execute-api.us-east-1.amazonaws.com/prod",
+  baseurl: "https://{API_GATEWAY_KEY_PROD}.execute-api.{inst_region}.amazonaws.com/prod",
   api_doc_name: "https://{api_doc_name}.s3.amazonaws.com",
   envName: "oss",
-  multi_env: true,
-  slack_support: true,
-  serviceTabs: ["overview", "access control", "metrics", "logs", "cost"],
-  environmentTabs: ["env_overview", "deployments", "code quality", "metrics", "assets", "env_logs"],
+  multi_env: {multi_env},
+  slack_support: {slack_support},
+  serviceTabs: ["{overview}", "{access control}", "{metrics}", "{logs}", "{cost}"],
+  environmentTabs: ["{env_overview}", "{deployments}", "{code quality}", "{metrics}", "{assets}", "{env_logs}"],
   urls: {
     docs_link: "https://github.com/tmobile/jazz/wiki",
     swagger_editor: "{swagger_editor}",
@@ -17,7 +17,7 @@ export const environment = {
     registrationMessage: 'Please contact your Jazz Admin(s) to get a registration code.'
   },
   aws: {
-    account_number:'192006145812',
-    region:"us-east-1",
+    account_number:'{account_number}',
+    region:"{region}",
   }
 };
