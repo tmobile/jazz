848efbb4fbf345d80c7fe10c4fc00759871c4a05
Michael Chinn <chnn.michael@gmail.com>
2018-09-17 16:33:12 -0700

    environments

--- core/jazz_ui/src/environments/environment.internal.ts
+++ core/jazz_ui/src/environments/environment.internal.ts
@@ -1,24 +1,23 @@
 export const environment = {
-    baseurl:"{base_url}",
-    envName:"internal",
-    multi_env:"{multi_env}",
-    serviceTabs:"{service_tabs}",
-    environmentTabs:"{environment_tabs}",
-    urls:{
-        privacyPolicy:"{{privacy_policy}}",
-        docs:"{{docs_link}}",
-        forgotPassword:"{{forgot_password}}",
-        signUp:"{{sign_up}}",
-        cloudapiURL:"{{cloud_api_url}}",
-        editor_cloudapiURL: "{{editor_cloud_api_url}}",
-        reportEmail:"{{report_email}}",
-        accounts:["{{accounts}}"],
-        regions:["{{regions}}"],
-        internal_acronym:"{{internal_acronym}}",
-        serverless_slack:"{{serverless_slack}}",
-        username_prefix:"{{username_prefix}}",
-        slack_messages:'{{slack_messages}}',
-    }
+  production: false,
+  configFile: 'config/config.prod.json',
+  baseurl: "https://cloud-api.corporate.t-mobile.com/api",
+  api_doc_name: 'http://cloud-api-doc.corporate.t-mobile.com',
+  envName : "jazz",
+  multi_env:false,
+  serviceTabs:['overview','access control','metrics','logs'],
+  environmentTabs:['overview','deployments','code quality','assets', 'metrics', 'logs','clear water'],
+  gaTrackingId: '',
+  urls:{
+    docs_link:"https://docs.jazz.corporate.t-mobile.com",
+    content_base: "https://docs.jazz.corporate.t-mobile.com/external-content",
+    swagger_editor: 'http://editor.cloud-api.corporate.t-mobile.com/?url='
+  },
+  userJourney: {
+    registrationMessage: ''
+  },
+  aws: {
+    account_number:'{account_number}',
+    region:"{region}",
+  }
 };
-
-
