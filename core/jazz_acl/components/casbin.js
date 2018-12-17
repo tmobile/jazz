f6e7fa6f72e34974b8e68b7fdad4b5e153f22b64
Siddharth Vidhani <svidhani@yahoo.com>
2018-12-17 23:35:53 +0530

    Added ACL implementation

--- core/jazz_acl/components/casbin.js
+++ core/jazz_acl/components/casbin.js
@@ -17,55 +17,165 @@
 const Enforcer = require('casbin').Enforcer;
 const TypeORMAdapter = require('typeorm-adapter');
 const logger = require('./logger.js');
+const errorHandlerModule = require("./error-handler.js")();
 
+/* Create a connection to the DB*/
 async function dbConnection(config) {
-  const conn = await TypeORMAdapter.default.newAdapter({
-    type: config.CASBIN.TYPE,
-    host: config.CASBIN.HOST,
-    port: config.CASBIN.PORT,
-    username: config.CASBIN.USER,
-    password: config.CASBIN.PASSWORD,
-    database: config.CASBIN.DATABASE,
-    timeout: config.CASBIN.TIMEOUT
-  });
+  let conn;
+  try {
+    conn = await TypeORMAdapter.default.newAdapter({
+      type: config.CASBIN.TYPE,
+      host: config.CASBIN.HOST,
+      port: config.CASBIN.PORT,
+      username: config.CASBIN.USER,
+      password: config.CASBIN.PASSWORD,
+      database: config.CASBIN.DATABASE,
+      timeout: config.CASBIN.TIMEOUT
+    });
+  } catch(err) {
+    if (err.name !== "AlreadyHasActiveConnectionError") {
+      logger.error(err.message);
+      throw (errorHandlerModule.throwInternalServerError(err.message));
+    }
+  }
 
-  const enforcer = await Enforcer.newEnforcer('../config/rbac_model.conf', conn);
-  return enforcer;
+  return conn;
 }
 
-async function addPermissionForUser(serviceId, policies) {
-  //TODO implement addPermissionForUser
+/* Get the policies from casbin given the serviceId*/
+async function getPolicies(serviceId, config) {
+  const values = [`${serviceId}_manage`, `${serviceId}_code`, `${serviceId}_deploy`];
+  const result = await getFilteredPolicy(1, values, config);
+  if (result && result.success) {
+    let filteredPolicies = result.filter(el => el.length >=1);
+    if (filteredPolicies.length) {
+      filteredPolicies = filteredPolicies.map(policyArr => policyArr.map(policy => [policy[0], policy[1].split('_')[1], policy[2]]));
+      result = filteredPolicies;
+    } else {
+      result = [];
+    }
+  }
 
-  return true;
-}
-
-async function enforce(policy) {
-
-  //TODO implement the enforce method
-  return true;
-}
-
-/* Get the policies from casbin given the value*/
-async function getFilteredPolicy(index, values, config) {
-  //TODO Implement getFilteredPolicy
   return result;
 }
 
-async function removeFilteredPolicy(index, value) {
-  //TODO: implement removeFilteredPolicy
+/* Get the policies from casbin given the values and index*/
+async function getFilteredPolicy(index, values, config) {
+  let result = {};
+  let conn, enforcer;
 
-  return true;
+  try {
+    conn = await dbConnection(config);
+    enforcer = await Enforcer.newEnforcer("./config/rbac_model.conf", conn);
+
+    const promisedPolicies = values.map(async value => await enforcer.getFilteredPolicy(index, value));
+    const policies = await Promise.all(promisedPolicies);
+    result = policies;
+  } catch(err) {
+    logger.error(err.message);
+    result.error = err.message;
+  } finally {
+    await conn.close();
+  }
+
+  return result;
 }
 
-async function getPermissionForUser(user) {
-  //TODO: implement getPermissionForUser
+/* Check permissions for a user */
+async function checkPermissions(userId, serviceId, category, permission, config) {
+  let result = {};
 
-  return [];
+  try {
+    const conn = await dbConnection(config);
+    const enforcer = await Enforcer.newEnforcer('./config/rbac_model.conf', conn);
+    result.authorized = enforcer.enforce(userId, `${serviceId}_${category}`, permission);
+  } catch(err) {
+    logger.error(err.message);
+    result = {
+      authorized: false,
+      error: err.message
+    };
+  } finally {
+    await conn.close();
+  }
+
+  return result;
+}
+
+/* Add and/or Remove filtered policy */
+async function addOrRemovePolicy(serviceId, config, action, policies) {
+  let result = {};
+  let conn;
+  const objects = [`${serviceId}_manage`, `${serviceId}_code`, `${serviceId}_deploy`];
+  let totalPolices = 0;
+  try {
+    const getPolicies = await getFilteredPolicy(1, objects, config);
+
+    if (getPolicies) {
+      totalPolicies = getPolicies.length;
+      let removeResult = [];
+
+      if (totalPolicies) {
+        conn = await dbConnection(config);
+        const enforcer = await Enforcer.newEnforcer('./config/rbac_model.conf', conn);
+
+        // remove (incase of add remove first)
+        if (action === 'remove' || action === 'add') {
+          removeResult = await getPolicies.map(async policies => {
+            return policies.map(async policy => {
+              await enforcer.removePolicy(policy[0], policy[1], policy[2]);
+            });
+          });
+          if (removeResult.length === totalPolicies) {
+            if (action === 'remove') {
+              await enforcer.savePolicy();
+            }
+          } else {
+            result.error = `Rollback transaction - could delete ${removeResult.length} of ${totalPolices} policies`;
+          }
+        }
+
+        // add (after remove)
+        if (action === 'add' && removeResult.length === totalPolicies) {
+          let savedPolicies = policies.map(async policy => await enforcer.addPolicy(policy.userId, `${serviceId}_${policy.category}`, policy.permission));
+          savedPolicies = await Promise.all(savedPolicies);
+          
+          if (savedPolicies.length === policies.length) {
+            await enforcer.savePolicy();
+          } else if (!savedPolicies.length) { //rollback deletion
+            result.error = `Rollback transaction - could not add any policy`;
+          } else if(savedPolicies.length !== policies.length) {
+            result.error = `Rollback transaction - could add ${savedPolicies.length} of ${policies.length}`;
+          } else {
+            result.error = `Rollback transaction - failed to add/remove policies`;
+          }
+        }
+      }
+    } else {
+      result.error = `No policies found for service id - ${serviceId}`;
+    }
+  } catch(err) {
+    logger.error(err.message);
+    result.error = err.message;
+  } finally {
+    if (totalPolicies) {
+      await conn.close();
+    }
+  }
+
+  return result;
+}
+
+/* Get the permissions for a user */
+async function getPolicyForServiceUser(serviceId, userId, config) {
+  const result = await getFilteredPolicy(1, serviceId, config);
+
+  return result;
 }
 
 module.exports = {
-  addPermissionForUser,
-  getFilteredPolicy,
-  removeFilteredPolicy,
-  addPermissionForUser
+  addOrRemovePolicy,
+  getPolicies,
+  getPolicyForServiceUser,
+  checkPermissions
 };
