071441b096730ce0e9c46667dc090eb54c12072a
Siddharth Vidhani <svidhani@yahoo.com>
2018-12-18 17:43:26 +0530

    Update jazzservices.po.ts

--- core/jazz_ui/e2e/page-objects/jazzservices.po.ts
+++ core/jazz_ui/e2e/page-objects/jazzservices.po.ts
@@ -31,121 +31,117 @@ export class Jazz {
     return element(by.css('div.service-box>div.icon-icon-function'));
   }
   getWebsite() {
-  return element(by.css('div.service-box>div.icon-icon-web'));
+    return element(by.css('div.service-box>div.icon-icon-web'));
   }
   getServiceName() {
     return element(by.css('input#serviceName'));
   }
   getNameSpace() {
-      return element(by.css('input#domainName'));
+    return element(by.css('input#domainName'));
   }
   getServiceDescription() {
-      return element(by.css('div.input-field-container>div.input-wrapper>textarea'));
+    return element(by.css('div.input-field-container>div.input-wrapper>textarea'));
   }
   getEventScheduleFixedRate() {
-      return element(by.css('div.radio-container>label[for="fixedRate"]'));
+    return element(by.css('div.radio-container>label[for="fixedRate"]'));
   }
   getAwsServiceName() {
-      return element(by.xpath('(//table-template//div[@class="table-row pointer"]/div)[1]'));
+    return element(by.xpath('(//table-template//div[@class="table-row pointer"]/div)[1]'));
   }
   getAPIType() {
-	  return element(by.xpath('//div[@class="table-row pointer"]//div[text()="api"]'));
+    return element(by.xpath('//div[@class="table-row pointer"]//div[text()="api"]'));
   }
   getFunctionType() {
-      return element(by.xpath('//div[@class="table-row pointer"]//div[text()="function"]'));
+    return element(by.xpath('//div[@class="table-row pointer"]//div[text()="function"]'));
   }
   getWebsiteType() {
-      return element(by.xpath('//div[@class="table-row pointer"]//div[text()="website"]'));
+    return element(by.xpath('//div[@class="table-row pointer"]//div[text()="website"]'));
   }
   getAPIStatus() {
-      return element(by.xpath('//div[@class="table-row pointer"]/div[text()="api"]/parent::div/div[5]'));
+    return element(by.xpath('//div[@class="table-row pointer"]/div[text()="api"]/parent::div/div[5]'));
   }
   getFunctionStatus() {
-      return element(by.xpath('//div[@class="table-row pointer"]/div[text()="function"]/parent::div/div[5]'));
+    return element(by.xpath('//div[@class="table-row pointer"]/div[text()="function"]/parent::div/div[5]'));
   }
   getWebsiteStatus() {
-      return element(by.xpath('//div[@class="table-row pointer"]/div[text()="website"]/parent::div/div[5]'));
+    return element(by.xpath('//div[@class="table-row pointer"]/div[text()="website"]/parent::div/div[5]'));
   }
   getDummy() {
-      return element(by.xpath('//*[@id="exampleName"]'));
+    return element(by.xpath('//*[@id="exampleName"]'));
   }
   getSubmit() {
-     return element(by.css('section.submit-form>button.btn'));
+    return element(by.css('section.submit-form>button.btn'));
   }
   getDone() {
-     return element(by.css('section.footer-btn>btn-jazz-primary'));
-    }
-  getServiceNameHeader() {
-     
-	 return element(by.xpath('//div[@class="page-title-wrap hide-for-mobile"]/h1[@class="page-hdr bold-title"]'));
+    return element(by.css('section.footer-btn>btn-jazz-primary'));
   }
-  getProdName(){
+  getServiceNameHeader() {
+    return element(by.xpath('//div[@class="page-title-wrap hide-for-mobile"]/h1[@class="page-hdr bold-title"]'));
+  }
+  getProdName() {
     return element(by.xpath('//div[@class="stage-title stageDisp" and contains(text(),"prod")]'))
 	}
-  getProdHeader(){
+  getProdHeader() {
     return element(by.xpath('//div[@class="servie-details-container"]/h1[@class="page-hdr bold-title relative" and contains(text(),"prod")]'));
   }
-  getAsset(){
+  getAsset() {
     return element(by.xpath('//li[@class="x caps" and contains(text(),"assets")]'));
   }
-  getAssetHeader(){
+  getAssetHeader() {
     return element(by.xpath('//div/div[2]/div/section[1]/div/div[2]/section/env-assets-section/div/div[1]/div[2]/div[1]/div[1]/span[2]'));
   }
-  getRefresh(){ 
+  getRefresh() { 
     return element(by.xpath('*//div[@class="refresh-button"]'));
   }
-  getServiceFromAsset(){
+  getServiceFromAsset() {
     return element(by.xpath('//div[@class="navigation-item"]/span[@class="icon-icon-back hide"]/following-sibling::a[text()="Services"]'));
   } 
-  getWebsiteName(){
+  getWebsiteName() {
     return element(by.xpath('//div[@class="table-row pointer"]/div[text()="website"]/preceding-sibling::div'));  
   }
-  getDeploymentStatus(){
+  getDeploymentStatus() {
     return element(by.xpath('//li[@class="x caps" and contains(text(),"deployments")]'));  
   }
-  getDeploymentStatusVerify(){
+  getDeploymentStatusVerify() {
     return element(by.xpath('//div[@class="column status-col"]/div[@class="green"]'));
   }
-  getAssetStatusVerify(){
+  getAssetStatusVerify() {
     return element(by.xpath('//div[@class="det-value"]'));
   }
-  getLamdaName(){
+  getLamdaName() {
     return element(by.xpath('//div[@class="table-row pointer"]/div[text()="function"]/preceding-sibling::div'));
   }
-  getOverviewStatus(){
+  getOverviewStatus() {
     return element(by.xpath('//li[@class="x caps active" and contains(text(),"overview")]'));  
   }
-  getLogs(){
+  getLogs() {
     return element(by.xpath('//li[@class="x caps" and contains(text(),"logs")]'));
   }
-  getFilterIcon(){
-	 return element(by.xpath('//div[@class="filter-icon relative"]'));
+  getFilterIcon() {
+    return element(by.xpath('//div[@class="filter-icon relative"]'));
   }
-  getDay(){
-   return element(by.xpath('//div[@class="dropdown open"]/ul[@class="dropdown-menu"]/li/a[(text()="Day")]'));
+  getDay() {
+    return element(by.xpath('//div[@class="dropdown open"]/ul[@class="dropdown-menu"]/li/a[(text()="Day")]'));
   }
-  getWeek(){
+  getWeek() {
     return element(by.xpath('//div[@class="dropdown open"]/ul[@class="dropdown-menu"]/li/a[(text()="Week")]'));
   }
-  getMonth(){
+  getMonth() {
     return element(by.xpath('//div[@class="dropdown open"]/ul[@class="dropdown-menu"]/li/a[(text()="Month")]'));
   }
-  getYear(){
+  getYear() {
     return element(by.xpath('//div[@class="dropdown open"]/ul[@class="dropdown-menu"]/li/a[(text()="Year")]'));
   }
-  getDropDown(){
+  getDropDown() {
     return element(by.xpath('//div[text()="TIME RANGE"]/following-sibling::dropdown[@title="Select range"]/div[@class="dropdown"]/button[@class="btn dropdown-btn dropdown-toggle"]'));
   }
-  getWeekVerify()
-  {
-	return element(by.xpath('//div[@class="row"]/div[contains(text(),"Time Range")]/b[text()="Week"]'));
+  getWeekVerify() {
+    return element(by.xpath('//div[@class="row"]/div[contains(text(),"Time Range")]/b[text()="Week"]'));
   }
-  getMonthVerify()
-  {
-	return element(by.xpath('//div[@class="row"]/div[contains(text(),"Time Range")]/b[text()="Month"]'));
+  getMonthVerify() {
+    return element(by.xpath('//div[@class="row"]/div[contains(text(),"Time Range")]/b[text()="Month"]'));
   }
-  getYearVerify()
-  {
-	return element(by.xpath('//div[@class="row"]/div[contains(text(),"Time Range")]/b[text()="Year"]'));
+  getYearVerify() {
+    return element(by.xpath('//div[@class="row"]/div[contains(text(),"Time Range")]/b[text()="Year"]'));
   }
  }
