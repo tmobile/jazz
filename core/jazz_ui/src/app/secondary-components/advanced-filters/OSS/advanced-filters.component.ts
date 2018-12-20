090dbef8178ed7ec4557096a2105c648534abe66
gaurav <gk.verma@outlook.com>
2018-12-20 10:26:26 +0530

    updated file

--- core/jazz_ui/src/app/secondary-components/advanced-filters/OSS/advanced-filters.component.ts
+++ core/jazz_ui/src/app/secondary-components/advanced-filters/OSS/advanced-filters.component.ts
@@ -84,6 +84,18 @@ export class AdvancedFiltersComponentOSS implements OnInit {
         this.periodSelected=this.periodList[0];
     }
 
+    setSlider(event){
+        this.sliderMax=event;
+        // update time period list when sliderMax changes
+        var timePeriodList = [];
+        for (var i = this.sliderFrom; i <= this.sliderMax; i ++){
+            timePeriodList.push(i);
+        }
+        this.timePeriodList = timePeriodList;
+        this.sliderFrom=1;
+        this.sliderPercentFrom=0;
+    }
+
     onRangeListSelected(range){
         this.selectedTimeRange = range;
         this.selectFilter["key"]='range';
