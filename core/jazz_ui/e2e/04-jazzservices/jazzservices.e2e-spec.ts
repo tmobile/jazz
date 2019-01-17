2dcafae6e63db57c26a40497cc614e9719556213
ElizabethJohnson <elizabeth.johnson@ust-global.com>
2019-01-17 13:34:15 +0530

    Minor change

--- core/jazz_ui/e2e/04-jazzservices/jazzservices.e2e-spec.ts
+++ core/jazz_ui/e2e/04-jazzservices/jazzservices.e2e-spec.ts
@@ -201,7 +201,7 @@ describe('Overview', () => {
             browser.driver.switchTo().activeElement();
             browser.sleep(15000);
             jazzServices_po.getMetrices().click();
-            Expect(jazzServices_po.getMetricesCount().getText()).toEqual('1');
+            expect(jazzServices_po.getMetricesCount().getText()).toEqual('1');
             browser.sleep(2000);
             jazzServices_po.getServiceFromAsset().click();            
       });
