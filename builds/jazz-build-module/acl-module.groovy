f6ce648f8bd8fc039f0182e9b1ab4dcc1b4af109
Suprita S T <suprita@moonraft.com>
2019-01-18 01:57:52 +0530

    bug fix

--- builds/jazz-build-module/acl-module.groovy
+++ builds/jazz-build-module/acl-module.groovy
@@ -38,7 +38,7 @@ def updateServiceACL(serviceId, auth_token, aclUrl, user) {
 		}
 	} catch (ex) {
     echo "ex: $ex"
-    error ex.message()
+    error ex.getMessage()
   }
 }
 
@@ -64,7 +64,7 @@ def deletePolicies(serviceId, auth_token, aclUrl) {
 		}
 	} catch(ex) {
 		echo "ex: $ex"
-		error ex.message()
+		error ex.getMessage()
 	}
 }
 
