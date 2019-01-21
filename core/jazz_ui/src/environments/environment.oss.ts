05b675545fa2176d16237e82bf121c1fcc48e3d9
gaurav <gk.verma@outlook.com>
2019-01-21 23:34:42 +0530

    updated

--- core/jazz_ui/src/environments/environment.oss.ts
+++ core/jazz_ui/src/environments/environment.oss.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   INSTALLER_VARS: {INSTALLER_VARS},
   configFile: 'config/config.oss.json',
-  baseurl: "https://uktaroef9c.execute-api.us-east-1.amazonaws.com/prod",
+  baseurl: "https://{API_GATEWAY_KEY_PROD}.execute-api.{inst_region}.amazonaws.com/prod",
   api_doc_name: "https://{api_doc_name}.s3.amazonaws.com",
   envName: "oss",
   multi_env: {multi_env},
--- core/jazz_ui/src/environments/environment.ts
+++ core/jazz_ui/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production:false,
     configFile: 'config/config.prod.json',
-    baseurl: "https://uktaroef9c.execute-api.us-east-1.amazonaws.com/prod",
+    baseurl: "https://{API_GATEWAY_KEY_PROD}.execute-api.{inst_region}.amazonaws.com/prod",
     api_doc_name : "https://{api_doc_name}.s3.amazonaws.com",
     envName:"jazz",
     multi_env:false,
