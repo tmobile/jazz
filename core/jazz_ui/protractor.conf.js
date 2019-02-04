2beca88f9132126eb5f1cf854788e3d4a9f8ef6a
Vijay Pandey <U93401@UST-PCI.COM>
2019-02-04 14:36:28 +0530

    missed some changes..modifying the the code

--- core/jazz_ui/protractor.conf.js
+++ core/jazz_ui/protractor.conf.js
@@ -23,7 +23,7 @@ exports.config = {
     'browserName': 'chrome',
     'chromeOptions': {
       // Get rid of --ignore-certificate yellow warning
-      args: ['--disable-gpu', '--no-sandbox', '--test-type=browser'],
+      args: ['--headless','--disable-gpu', '--no-sandbox', '--test-type=browser'],
       // Set download path and avoid prompting for download even though
       // this is already the default on Chrome but for completeness
       prefs: {
