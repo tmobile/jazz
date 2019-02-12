1d9b77f4b2602f38aac0dd2b096d888bb4b17a64
supritat <33751253+supritat@users.noreply.github.com>
2019-02-12 13:45:49 +0530

    :bugfix - cognito-token-authorizer (#828)
    updated service name to satisfy length restriction

--- core/jazz_token-authorizer/index.js
+++ core/jazz_token-authorizer/index.js
@@ -14,16 +14,14 @@
 // limitations under the License.
 // =========================================================================
 
-'use strict';
 const errorHandlerModule = require("./components/error-handler.js");
-
 const configModule = require("./components/config.js");
 const logger = require("./components/logger.js");
+
 const jwt = require("jsonwebtoken");
 const request = require('request');
 const jwkToPem = require('jwk-to-pem');
 const AWS = require('aws-sdk');
-const _ = require('lodash');
 
 var pems;
 var cognitoUserPoolEndpoint;
@@ -170,7 +168,7 @@ module.exports.handler = function (event, context, cb) {
                             resource += apiGatewayArnTmp[3];
                         }
                         //For more information on specifics of generating policy, refer to blueprint for API Gateway's Custom authorizer in Lambda console
-                        var policy = new AuthPolicy(emailAddress.Value, awsAccountId, apiOptions);
+                        var policy = new AuthPolicy(emailAddress[0].Value, awsAccountId, apiOptions);
                         policy.allowAllMethods();
                         context.succeed(policy.build());
                     }
