6c9f5e208290d4ffbeca9e38d4454ec9b851d3d9
Heina Chang <heina.chang@dev9.com>
2019-02-19 17:18:50 -0800

    removed not in used Event folder

--- builds/azure-config-pack/Event/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context, event) {
-     var timeStamp = new Date().toISOString();
- 
-      if(event.isPastDue)
-     {
-         context.log('JavaScript is running late!');
-     }
-     context.log('JavaScript timer trigger function ran!', timeStamp);
- }; 
