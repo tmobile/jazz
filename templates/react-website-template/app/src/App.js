a508a121d6fde4595aec34c4735eef9c93668997
pgobin <pgobin@users.noreply.github.com>
2019-03-21 13:24:26 -0700

    Prefer web template framework boilerplate (#927)
    - Use boilerplate as shipped for web templates
    - Bump framework versions
    - Update UX
    
    Signed-off-by: PGobz <p.gobin@gmail.com>

--- templates/react-website-template/app/src/App.js
+++ templates/react-website-template/app/src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import Contents from './components/Contents';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './components/contents.css'
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="header">
-        <Header />
-        <Contents />
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Welcome to my React app
+          </h1>
+          <div className="App-logo">
+            <img height="130" width="180" src={logo} alt="logo" />
+            <h3>Powered by Jazz</h3>
+          </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
