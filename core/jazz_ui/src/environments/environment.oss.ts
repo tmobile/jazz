e595a9e5c4d226a58bbdac315d7f7c45332523a4
Swati Shreya <swati@piktorlabs.com>
2019-03-25 14:38:12 +0530

    Removed hard coded

--- core/jazz_ui/src/environments/environment.oss.ts
+++ core/jazz_ui/src/environments/environment.oss.ts
@@ -1,15 +1,14 @@
 export const environment = {
   production: true,
-  INSTALLER_VARS: { "environment_tabs": { "overview": true, "code_quality": "false", "deployments": true, "assets": true, "logs": true }, "CREATE_SERVICE": { "DEPLOYMENT_TARGETS": { "API": { "options": [{ "label": "AWS API Gateway", "value": "aws_apigateway" }, { "label": "GCP APIGEE", "value": "gcp_apigee" }], "active": true }, "FUNCTION": { "options": [{ "label": "AWS Lambda", "value": "aws_lambda" }], "active": false }, "WEBSITE": { "options": [{ "label": "AWS S3", "value": "aws_s3" }, { "label": "AWS Cloudfront", "value": "aws_cloudfront" }], "active": false } } }, "feature": { "multi_env": true, "apigee": "false" }, "service_tabs": { "overview": true, "cost": false, "access_control": true, "metrics": true, "logs": true } },
+  INSTALLER_VARS: {INSTALLER_VARS},
   configFile: 'config/config.oss.json',
-  baseurl: "https://dbn9ruqpn3.execute-api.us-east-1.amazonaws.com/prod",
-  api_doc_name: "https://multi0312-jazz-s3-api-doc-20190312053238719000000007.s3.amazonaws.com",
+  baseurl: "https://{API_GATEWAY_KEY_PROD}.execute-api.{inst_region}.amazonaws.com/prod",
+  api_doc_name: "https://{api_doc_name}.s3.amazonaws.com",
   envName: "oss",
   multi_env: true,
-  slack_support: true,
-  envLists: { "nodejs8.10": "Nodejs 8.10", "python2.7": "Python 2.7", "python3.6": "Python 3.6", "java8": "Java 8", "go1.x": "Go 1.x" },
-  serviceTabs: ["overview", "{access control}", "metrics", "logs", "{cost}"],
-  environmentTabs: ["overview", "deployments", "{code quality}", "metrics", "assets", "logs"],
+  envLists:  {"nodejs8.10": "Nodejs 8.10", "python2.7": "Python 2.7", "python3.6": "Python 3.6", "java8": "Java 8", "go1.x": "Go 1.x"},
+  serviceTabs: ["{overview}", "{access control}", "{metrics}", "{logs}", "{cost}"],
+  environmentTabs: ["{env_overview}", "{deployments}", "{code quality}", "{metrics}", "{assets}", "{env_logs}"],
   charachterLimits: {
     eventMaxLength: {
       "stream_name": 128,
@@ -41,5 +40,5 @@ export const environment = {
     account_number: '{account_number}',
     region: "{region}",
   },
-  accountMap: [{ "primary": true, "account": "102707241671", "regions": ["us-east-1"] }, { "primary": false, "account": "108206174331", "regions": ["us-west-2"] }]
+  accountMap:{accountMap}
 };
