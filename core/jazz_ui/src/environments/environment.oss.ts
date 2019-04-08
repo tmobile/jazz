a319cb51274cda2b534bffb1c69a6f181e09abe9
swati-sh <swati@piktorlabs.com>
2019-04-08 11:50:15 +0530

    no message

--- core/jazz_ui/src/environments/environment.oss.ts
+++ core/jazz_ui/src/environments/environment.oss.ts
@@ -1,58 +1,15 @@
 export const environment = {
   production: true,
-  INSTALLER_VARS: {
-    "CREATE_SERVICE": {
-      "DEPLOYMENT_TARGETS": {
-          "API": {
-              "active": true,
-              "options": [
-                  {"label": "AWS API Gateway", "value": "aws_apigateway"},
-                  {"label": "GCP APIGEE", "value": "gcp_apigee"}
-              ]
-          },
-          "WEBSITE": {
-              "active": false,
-              "options": [
-                  {"label": "AWS S3", "value": "aws_s3"},
-                  {"label": "AWS Cloudfront", "value": "aws_cloudfront"}
-              ]
-          },
-          "FUNCTION": {
-              "active": false,
-              "options": [
-                  {"label": "AWS Lambda", "value": "aws_lambda"}
-              ]
-          }
-      }
-    },
-    "feature": {
-        "multi_env": true,
-        "apigee": "{apigee_support}"
-    },
-    "service_tabs": {
-        "overview": true,
-        "access_control": true,
-        "metrics": true,
-        "logs": true,
-        "cost": false
-    },
-    "environment_tabs": {
-        "overview": true,
-        "deployments": true,
-        "code_quality": "false",
-        "logs": true,
-        "assets": true
-    }
-},
+  INSTALLER_VARS:{INSTALLER_VARS},
   configFile: 'config/config.oss.json',
-  baseurl: "https://lp5zztke62.execute-api.us-east-1.amazonaws.com/prod",
-  api_doc_name: "https://slsapp19-jazz-s3-api-doc-20190405100217092500000006.s3.amazonaws.com",
+  baseurl:  "https://{API_GATEWAY_KEY_PROD}.execute-api.{inst_region}.amazonaws.com/prod",
+  api_doc_name: "https://{api_doc_name}.s3.amazonaws.com",
   envName: "oss",
-  multi_env: true,
-  slack_support: false,
+  multi_env: {multi_env},
+  slack_support: {slack_support},
   envLists:  {"nodejs8.10": "Nodejs 8.10", "python2.7": "Python 2.7", "python3.6": "Python 3.6", "java8": "Java 8", "go1.x": "Go 1.x"},
-  serviceTabs: ["overview", "access control", "metrics", "logs", "{cost}"],
-  environmentTabs: ["overview", "deployments", "{code quality}", "metrics", "assets", "logs"],
+  serviceTabs: ["{overview}", "{access control}", "{metrics}", "{logs}", "{cost}"],
+  environmentTabs: ["{env_overview}", "{deployments}", "{code quality}", "{metrics}", "{assets}", "{env_logs}"],
   assetTypeList:[
 		"lambda",
 		"apigateway",
@@ -94,7 +51,7 @@ export const environment = {
     registrationMessage: 'Please contact your Jazz Admin(s) to get a registration code.'
   },
   aws: {
-    account_number:'102707241671',
-    region:"us-east-1"
+    account_number:'{account_number}',
+    region:"{region}"
   }
 };
