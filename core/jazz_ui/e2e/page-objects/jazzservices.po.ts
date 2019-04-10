30682c4ad661bd6b3db82a6a16c27948f3100b2c
Vijay Pandey <U93401@UST-PCI.COM>
2019-04-10 12:36:24 +0530

    updated the xpath

--- core/jazz_ui/e2e/page-objects/jazzservices.po.ts
+++ core/jazz_ui/e2e/page-objects/jazzservices.po.ts
@@ -148,11 +148,7 @@ export class Jazz {
     return element(by.xpath('//span[@title="Refresh"]'));
   }
   serviceStatus(servicename) {
-<<<<<<< HEAD
     return element(by.xpath('//div/div[@class="table-row pointer"]/div[text()="'+servicename+'"]/parent::div/div[contains(text(), "active")]'));
-=======
-    return element(by.xpath('//div/div[@class="table-row pointer"]/div[text()="' + servicename + '"]/parent::div/div[contains(text(), "active")]'));
->>>>>>> upstream/ui_automation
   }
   getMetrices() {
     return element(by.xpath('//li[contains(text(),"metrics")]'));
@@ -375,14 +371,10 @@ export class Jazz {
   getTestBranchName() {
     return element(by.xpath('//div[@class="stage-title2 stageDisp"]'));
   }
-<<<<<<< HEAD
   getLoginSpinner()
   {
     return element(by.css('div.loader'));
   }
-=======
-
->>>>>>> upstream/ui_automation
   getSpinner() {
     return element(by.css('div.loading-circle'));
   }
@@ -401,14 +393,8 @@ export class Jazz {
   getGitLogout() {
     return element(by.xpath('//a[@class="sign-out-link"]'));
   }
-<<<<<<< HEAD
   getService(servicename) { 
     return element(by.xpath('(//table-template//div[@class="table-row pointer"]/div)[contains(text(),"'+servicename+'")]'));  
     }
-=======
-  getService(servicename) {
-    return element(by.xpath('(//table-template//div[@class="table-row pointer"]/div)[contains(text(),"' + servicename + '")]'));
-  }
->>>>>>> upstream/ui_automation
 
 }
