d40af5bc1abcc07913fb88212302534beb008adb
Akhil K <akhil.kn@pitsolutions.com>
2019-04-18 15:04:10 +0530

    conflict resolve

--- core/jazz_services/components/crud/getList.js
+++ core/jazz_services/components/crud/getList.js
@@ -23,66 +23,6 @@
 **/
 
 const utils = require("../utils.js")();
-const _ = require("lodash");
-
-module.exports = (query, getAllRecords, onComplete) => {
-    // initialize dynamodb
-    var dynamodb = utils.initDynamodb();
-
-    var filter = "";
-    var attributeValues = {};
-    var insertAnd = " AND ";
-
-    var scanparams = {
-        "TableName": global.services_table,
-        "ReturnConsumedCapacity": "TOTAL",
-        "Limit": "500"
-    };
-
-    var filter_key = utils.getDatabaseKeyName(global.config.service_filter_key);
-
-    if (query !== undefined && query !== null) {
-
-        var keys_list = global.config.service_filter_params;
-
-        keys_list.forEach(function (key) {
-
-            var key_name = utils.getDatabaseKeyName(key);
-
-            if (key_name == "SERVICE_TIMESTAMP" && (query.last_updated_after !== undefined || query.last_updated_before !== undefined)) {
-                filter = filter + key_name + " BETWEEN :BEFORE" + " AND :AFTER " + insertAnd;
-                attributeValues[(":BEFORE")] = {
-                    'S': query.last_updated_before
-                };
-                attributeValues[(":AFTER")] = {
-                    'S': query.last_updated_after
-                };
-            } else if (key_name == "SERVICE_STATUS" && query.status !== undefined) {
-                var status = query.status;
-                var array = status.split(',');
-                var obj = {};
-
-                var filterString = "( ";
-                array.forEach(function (value) {
-                    filterString += " :" + value + " , ";
-                });
-                filterString = filterString.substring(0, filterString.length - 3);
-                filterString += " )";
-
-                filter = filter + key_name + " IN " + filterString + " AND ";
-                array.forEach(function (value) {
-                    attributeValues[(":" + value)] = {
-                        'S': value
-                    };
-                });
-            } else if (query[key]) {
-                filter = filter + `contains(${key_name}, :${key_name})` + insertAnd;
-                attributeValues[(":" + key_name)] = {
-                    'S': query[key]
-                };
-            }
-        });
-    }
 
 module.exports = (query, servicesList, onComplete) => {
   // initialize dynamodb
