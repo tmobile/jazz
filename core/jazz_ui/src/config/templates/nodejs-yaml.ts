10bbf185b48073a2103f4f201fe74505de816349
swati-sh <swati@piktorlabs.com>
2019-05-29 20:34:33 +0530

    Deployment-descriptor

--- core/jazz_ui/src/config/templates/nodejs-yaml.ts
+++ core/jazz_ui/src/config/templates/nodejs-yaml.ts
@@ -1,8 +1,34 @@
 export const nodejsTemplate = {
     template:
-`functions: 
-    function1: 
-      handler: function1/index.handler
-    function2: 
-      handler: function2/index.handler`
+`service:
+  name:sls
+provider:
+  name: aws
+  region: us-east-1
+stackTags:
+  application: sls190521
+  service: testplugins
+  domain: testjazz
+  owner: serverless@t-mobile.com
+  environment: prod
+functions: 
+  function1: 
+    handler: function1/index.handler
+    events:
+      - s3: 
+          bucket:s3bucket
+          events:s3events
+  function2:
+    handler: function2/index.handler
+resources:
+  Resources:
+    myresources:
+      Type: AWS::DynamoDB::Table
+      Properties:
+        TableName: table-name
+    notherresources:
+      Type: AWS::DynamoDB::Table
+      Properties:
+        TableName: table-nameres
+  `
 };
