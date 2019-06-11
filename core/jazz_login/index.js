26e437c6d067a0959f37198b0bcd8b4d7c6667e8
Sini Wilson <sini.wilson@ust-global.com>
2019-06-11 22:49:02 +0530

    Fixing fetch is not defined issue

--- core/jazz_login/index.js
+++ core/jazz_login/index.js
@@ -16,6 +16,7 @@
 
 'use strict';
 
+global.fetch = require('node-fetch');
 const AWSCognito = require('amazon-cognito-identity-js');
 
 const errorHandlerModule = require("./components/error-handler.js");
