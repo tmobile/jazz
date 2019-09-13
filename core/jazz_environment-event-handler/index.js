f213cc19ef9765e675cb0d44a48e5bf725fde83a
Prakash Raghothamachar <prakashr@airliftj.com>
2019-09-13 13:54:53 -0700

    porting jazz_environment-event-handler from internal.

--- core/jazz_environment-event-handler/index.js
+++ core/jazz_environment-event-handler/index.js
@@ -225,6 +225,11 @@ function manageProcessItem(eventPayload, serviceDetails, configData, authToken) 
       environmentApiPayload.status = svcContext.status;
       environmentApiPayload.endpoint = svcContext.endpoint;
       environmentApiPayload.friendly_name = svcContext.friendly_name;
+      
+      // update the deployment_descriptor when available
+      if (svcContext.deployment_descriptor) {
+        environmentApiPayload.deployment_descriptor = svcContext.deployment_descriptor;
+      }
 
       if (svcContext.metadata) {
         environmentApiPayload.metadata = svcContext.metadata;
