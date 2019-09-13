d60e1edf98813d3ba29c9bf10170cc7b737cda18
Prakash Raghothamachar <prakashr@airliftj.com>
2019-09-13 13:47:16 -0700

    porting jazz_environment-event-handler changes from internal.

--- core/jazz_environment-event-handler/index.js
+++ core/jazz_environment-event-handler/index.js
@@ -196,7 +196,9 @@ function manageProcessItem(eventPayload, serviceDetails, configData, authToken) 
     environmentApiPayload.physical_id = svcContext.branch;
 
     if (eventPayload.EVENT_NAME.S === configData.EVENTS.INITIAL_COMMIT) {
-      environmentApiPayload.deployment_descriptor = serviceDetails.deployment_descriptor
+      if (serviceDetails.deployment_descriptor) {
+        environmentApiPayload.deployment_descriptor = serviceDetails.deployment_descriptor
+      }
       exportable.processEventInitialCommit(environmentApiPayload, serviceDetails.id, configData, authToken)
         .then((result) => { return exportable.processBuild(environmentApiPayload, serviceDetails, configData, authToken); })
         .then((result) => { return resolve(result); })
@@ -207,7 +209,9 @@ function manageProcessItem(eventPayload, serviceDetails, configData, authToken) 
 
     } else if (eventPayload.EVENT_NAME.S === configData.EVENTS.CREATE_BRANCH) {
       environmentApiPayload.friendly_name = svcContext.branch;
-      environmentApiPayload.deployment_descriptor = serviceDetails.deployment_descriptor
+      if (serviceDetails.deployment_descriptor) {
+        environmentApiPayload.deployment_descriptor = serviceDetails.deployment_descriptor
+      }
 
       exportable.processEventCreateBranch(environmentApiPayload, serviceDetails.id, configData, authToken)
         .then((result) => { return exportable.processBuild(environmentApiPayload, serviceDetails, configData, authToken); })
