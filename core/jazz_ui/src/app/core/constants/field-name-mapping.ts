7795b7ef7969f65fb1970792f4bf591b42251810
Dimple Patel <dimple@piktorlabs.com>
2019-09-26 19:46:05 +0530

    mapping added for asset types.

--- core/jazz_ui/src/app/core/constants/field-name-mapping.ts
+++ core/jazz_ui/src/app/core/constants/field-name-mapping.ts
@@ -6,5 +6,10 @@ export const MAPPING = {
     'dynamodb': 'DynamoDB',
     'endpoint_url': 'Endpoint URL',
     'swagger_url': 'Swagger URL',
-    'sequence_diagram_url': 'Sequence Diagram URL'
+    'sequence_diagram_url': 'Sequence Diagram URL',
+    'cloudwatch_event': 'CloudWatch Event',
+    'dynamodb_stream': 'DynamoDB Stream',
+    'kinesis_stream': 'Kinesis',
+    'dns_record': 'DNS Record',
+    'apigee_proxy': 'Apigee Proxy'
 };
