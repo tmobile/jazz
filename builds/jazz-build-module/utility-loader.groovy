087023b25afe7386bde1b557340fabd9f76c96fa
Sini Wilson <Sini.Wilson@ust-global.com>
2020-02-17 09:58:51 +0530

    fixing the formatting

--- builds/jazz-build-module/utility-loader.groovy
+++ builds/jazz-build-module/utility-loader.groovy
@@ -267,13 +267,13 @@ def createAllStackResources (whiteListModule, events, config, stackResources, en
 }
 
 def archiveCustomRole(assets_api, auth_token, config, env, events) {  
-  def assets = getAssets(assets_api, auth_token, config, env)
-  def assetList = parseJson(assets)
+def assets = getAssets(assets_api, auth_token, config, env)
+def assetList = parseJson(assets)
 
-  for (asset in assetList.data.assets) {
-	if (asset.asset_type == 'iam_role')
-      	events.sendCompletedEvent('UPDATE_ASSET', "Archiving the custom role since user specific role is being used.", generateAssetMap(asset.provider, asset.provider_id, "iam_role", config), env)
-  }
+for (asset in assetList.data.assets) {
+if (asset.asset_type == 'iam_role')
+	events.sendCompletedEvent('UPDATE_ASSET', "Archiving the custom role since user specific role is being used.", generateAssetMap(asset.provider, asset.provider_id, "iam_role", config), env)
+}
 }
 
 
