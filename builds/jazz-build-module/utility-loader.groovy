db463b31d0fb8073263575f33dcec11ceabd2202
Sini Wilson <Sini.Wilson@ust-global.com>
2020-03-17 18:40:46 +0530

    slslapp

--- builds/jazz-build-module/utility-loader.groovy
+++ builds/jazz-build-module/utility-loader.groovy
@@ -289,7 +289,7 @@ def archiveCustomRole(assets_api, auth_token, config, env, events) {
 	}
 }
 
-def archiveCustomRole(assets_api, auth_token, config, env, events) {  
+def archiveOldAssets(assets_api, auth_token, config, env, events) {  
 	def assets = getAssets(assets_api, auth_token, config, env)
 	def assetList = parseJson(assets)
 
