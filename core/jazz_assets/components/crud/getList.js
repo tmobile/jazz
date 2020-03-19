54e9a68dde18cc9cb81155019f0431d26edd48d3
Sini Wilson <Sini.Wilson@ust-global.com>
2020-03-19 14:42:43 +0530

    Making status as searchable

--- core/jazz_assets/components/crud/getList.js
+++ core/jazz_assets/components/crud/getList.js
@@ -96,8 +96,10 @@ module.exports = (query, asset_table, onComplete) => {
         docClient.query(params, (err, data) => {
             let count;
             if (err) {
+                logger.info("GETLIST err: "+ JSON.stringify(err));
                 onComplete(err, null);
             } else {
+                logger.info("GETLIST data: "+ JSON.stringify(data));
                 items = items.concat(data.Items);
                 if (data.LastEvaluatedKey) {
                     params.ExclusiveStartKey = data.LastEvaluatedKey;
