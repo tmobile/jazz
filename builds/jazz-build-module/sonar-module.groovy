c15afbdeac171cfbfef980cd6b9eb2bd65688e5b
Lethalmathematix <10322629+Lethalmathematix@users.noreply.github.com>
2021-02-13 21:32:43 +0530

    Refactor and move code coverage related changes to sonar-module.groovy

--- builds/jazz-build-module/sonar-module.groovy
+++ builds/jazz-build-module/sonar-module.groovy
@@ -22,6 +22,7 @@ echo "Sonar code analyzer module loaded successfully"
 @Field def g_sonar_project_properties
 @Field def g_dependencyCheckOutputFileName = "dependency-check-report.xml"
 @Field def g_dependency_check_properties = [:]
+@Field def g_sonar_exclusions = "**/dist/**,**/jazz-modules/**,**/virtualenv/**,**/venv/**,**/*.json,**/*.spec.js,**/vendor/**,**/pkg/**,**/library/**,**/node_modules/**,**/coverage/**,**/target/**"
 
 /**
  * Configure sonar and create the map for project specific sonar
@@ -41,6 +42,7 @@ def configureSonarProperties() {
 		g_sonar_project_properties["sonar.projectName"] = g_sonar_projectKey
 		g_sonar_project_properties["sonar.projectVersion"] = g_sonar_projectVersion
 		g_sonar_project_properties["sonar.sources"] = g_sonar_sources
+		g_sonar_project_properties["sonar.exclusions"] = g_sonar_exclusions
 
 		if (service_config['runtime'].indexOf("java") > -1) {
 			g_sonar_project_properties["sonar.java.binaries"] = g_sonar_java_binaries
@@ -50,7 +52,7 @@ def configureSonarProperties() {
 		if (service_config['runtime'].indexOf("go") > -1) {
 			g_sonar_project_properties["sonar.tests"] = "."
 			g_sonar_project_properties["sonar.test.inclusions"] = "**/*_test.go"
-			g_sonar_project_properties["sonar.go.coverage.reportPaths"] = "src/${g_sonar_projectKey}/bin/cov.out"
+			g_sonar_project_properties["sonar.go.coverage.reportPaths"] = "cov.out"
 		}
 
 		if (service_config['runtime'].indexOf("node") > -1) {
@@ -62,7 +64,7 @@ def configureSonarProperties() {
 		}
 
 		if (service_config['framework'] && service_config['framework'].indexOf("angular") > -1) {
-			g_sonar_project_properties["sonar.javascript.lcov.reportPaths"] = "app/coverage/${g_sonar_projectKey}/lcov.info"
+			g_sonar_project_properties["sonar.javascript.lcov.reportPaths"] = "app/coverage/lcov.info"
 		}
 
 		if (service_config['framework'] && service_config['framework'].indexOf("react") > -1) {
@@ -134,12 +136,13 @@ def setCredentials(username, password) {
  */
 def runReport() {
 	try {
+		cleanUpWorkspace()
+		generateCoverageReports()
 		def sonar_scanner_cl = "sonar-scanner"
 		for (item in g_sonar_project_properties) {
 			sonar_scanner_cl += " -D${item.key}=${item.value} "
 		}
 		jazz_quiet_sh(sonar_scanner_cl)
-		cleanUpWorkspace()
 	} catch (ex) {
 		error "runReport failed: " + ex.getMessage()
 	}
@@ -159,6 +162,113 @@ def cleanUpWorkspace() {
 	} else if (service_config['runtime'].indexOf("python") > -1) {
 		sh "rm -rf ./library"
 	}
+}
+
+/**
+* Run Unit Tests and generate Code Coverage Reports
+ */
+def generateCoverageReports() {
+	def domain = service_config['domain']
+    def repo_name = domain + "_" + service_config['service']
+	
+	if (service_config['runtime'].indexOf("nodejs") > -1) {
+		sh "npm install"
+		sh "npm run coverage"
+		sh "rm -rf ./node_modules"
+	} else if (service_config['runtime'].indexOf("java") > -1) {
+		sh "mvn verify > outputMvnVerify.log 2>&1"
+	} else if (service_config['runtime'].indexOf("python") > -1) {
+		// install requirements.txt in library folder, these python modules will be a part of deployment package
+		sh "mkdir library"
+		//Installing dependencies
+		sh "pip install -r requirements.txt -t library"
+		sh "touch library/__init__.py"
+		if (service_config['runtime'] == 'python3.6' || service_config['runtime'] == 'python3.8') {
+			// create virtual environment and install pytest
+			sh """
+			python3 -m venv virtualenv
+			. virtualenv/bin/activate
+			pip3 install pytest
+			pip3 install coverage
+			coverage run -m pytest --ignore=library
+			coverage xml -i
+			"""
+			sh "rm -rf ./library"
+		} else {
+			// create virtual environment and install pytest
+			sh """
+			pip install virtualenv
+			virtualenv venv
+			. venv/bin/activate
+			pip install pytest
+			pip install coverage
+			coverage run -m pytest --ignore=library
+			coverage xml -i
+			"""
+			sh "rm -rf ./library"
+		}
+	}  else if (service_config['runtime'].indexOf("go") > -1 ) {
+		// Installing dependencies using dep ensure
+		// golang build scripts
+		withEnv(["GOPATH=${env.WORKSPACE}"]) {
+			File functionPath = new File("${env.WORKSPACE}/${repo_name}/functions/");
+			if (functionPath.isDirectory()) {
+				sh "mkdir -p $GOPATH/src"
+				sh "rsync -a --exclude='.*' $GOPATH/" + repo_name + " $GOPATH/src"
+				sh "cd $GOPATH/src/"+ repo_name+" && dep ensure"
+				dirs = getFunctionPaths()
+				def size = dirs.size()
+				if( size > 0) {
+					for(item in dirs) {
+						def functionpath = "$GOPATH/src/${repo_name}/${item}"
+						sh "env GOOS=linux GOARCH=amd64 go build -o ${item}/main ${functionpath}/main.go"
+					}
+					sh "cd $GOPATH/src/" + repo_name + " && go test ./... -short -coverprofile=cov.out"
+					sh "cd $GOPATH/src/" + repo_name + " && go tool cover -func=cov.out"
+					sh "cp $GOPATH/src/" + repo_name + "/cov.out ${env.WORKSPACE}/${repo_name}/cov.out"
+				} else {
+					error "No Functions Found..skipping build"
+				}
+			} else {
+				sh "mkdir -p $GOPATH/src"
+				sh "rsync -a --exclude='.*' $GOPATH/" + repo_name + " $GOPATH/src"
+				sh "cd $GOPATH/src/"+ repo_name+" && dep ensure"
+				sh "cd $GOPATH/src/"+ repo_name+" && env GOOS=linux GOARCH=amd64 go build -o $GOPATH/"+ repo_name+"/main  *.go"
+				sh "cd $GOPATH/src/" + repo_name + " && go test -short -coverprofile=cov.out"
+				sh "cd $GOPATH/src/" + repo_name + " && go tool cover -func=cov.out"
+				sh "cp $GOPATH/src/" + repo_name + "/cov.out ${env.WORKSPACE}/${repo_name}/cov.out"
+			}
+		}
+	} else if (service_config['framework'] && service_config['framework'].indexOf("angular") > -1) {
+		sh """
+		cd app/
+		npm install
+		npm run test -- --no-watch --code-coverage > outputTest.log 2>&1
+		rm -rf ./node_modules
+		"""
+	} else if (service_config['framework'] && service_config['framework'].indexOf("react") > -1) {
+		sh """
+		cd app/
+		npm install
+		npm test -- --coverage > outputTest.log 2>&1
+		rm -rf ./node_modules
+		"""
+	}
+}
+
+/** 
+* Get list of paths for functions. They are expected in functions/ directory
+*/
+def getFunctionPaths() {
+  sh "ls -al"
+  def targetPaths = null
+  try {
+    targetPaths = sh(returnStdout: true, script: 'ls -d functions/*').split("\n")
+    echo "completed : $targetPaths"
+  } catch (ex) {
+      echo "Error occured while getting the functions list. Please make sure that your functions are available in functions/ directory"
+  }
+  return targetPaths
 }
 
 /**
